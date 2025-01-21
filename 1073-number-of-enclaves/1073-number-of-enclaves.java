c6abd5b8cb0b1ed2fd7e579b9ec6c5d1c1f3c71e
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-01-21 18:00:13 +0530

    Time: 10 ms (32.7%), Space: 62.5 MB (7.06%) - LeetHub

--- 1073-number-of-enclaves/1073-number-of-enclaves.java
+++ 1073-number-of-enclaves/1073-number-of-enclaves.java
@@ -3,35 +3,32 @@ class Solution {
         int n=arr.length;
         int m=arr[0].length;
         int vis[][]=new int[n][m];
-        
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if((i==0 || j==0||i==n-1||j==m-1) && arr[i][j]==1){
-                    // count[0]++;
-                    dfs(i,j,arr,vis);
+                if((i==0 || j==0 || i==n-1 || j==m-1) && arr[i][j]==1){
+                    dfs(i,j,n,m,vis,arr);
                 }
             }
         }
         int count=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(vis[i][j]!=-1&&arr[i][j]==1){
+                if(arr[i][j]==1 && vis[i][j]!=-1){
                     count++;
                 }
             }
         }
         return count;
     }
-    public static void dfs(int i,int j,int arr[][],int vis[][]){
+    public static void dfs(int i,int j,int n , int m ,int vis[][],int arr[][]){
         vis[i][j]=-1;
-        // count[0]--;
         int xCor[]={-1,0,0,1};
         int yCor[]={0,-1,1,0};
         for(int z=0;z<4;z++){
             int nextX=xCor[z]+i;
             int nextY=yCor[z]+j;
-            if(nextX>=0 && nextY>=0 && nextX<arr.length && nextY<arr[0].length && arr[nextX][nextY]==1 && vis[nextX][nextY]!=-1){
-                dfs(nextX,nextY,arr,vis);
+            if(nextX>=0 && nextY>=0 && nextX<n && nextY<m && arr[nextX][nextY]==1 && vis[nextX][nextY]==0){
+                dfs(nextX,nextY,n,m,vis,arr);
             }
         }
     }
