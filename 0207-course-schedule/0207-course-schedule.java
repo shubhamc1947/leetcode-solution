c8f3d4f7cb33ae2fa6743bbe69b66335ea93ceef
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-01-22 08:02:36 +0530

    Time: 6 ms (74.12%), Space: 44.8 MB (92.63%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,47 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] pre) {
+        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
+        for(int i=0;i<numCourses;i++){
+            adj.add(new ArrayList<>());
+        }
+
+        for(int i=0;i<pre.length;i++){
+            adj.get(pre[i][0]).add(pre[i][1]);
+        }
+        // System.out.println(adj);
+        // return true;
+
+        //inOrder Array 
+        int inOrder[]=new int[adj.size()];
+        for(int i=0;i<adj.size();i++){
+            for(int j=0;j<adj.get(i).size();j++){
+                int ele=adj.get(i).get(j);
+                inOrder[ele]++;
+            }
+        }
+
+        // queue making for putting 0 inorder ele
+        Queue<Integer> que=new LinkedList<>();
+        for(int i=0;i<inOrder.length;i++){
+            if(inOrder[i]==0){
+                que.add(i);
+            }
+        }
+
+        // ans
+        ArrayList<Integer> ans=new ArrayList<>();
+        while(!que.isEmpty()){
+            int node=que.remove();
+            ans.add(node);
+            for(int i=0;i<adj.get(node).size();i++){
+                int ele=adj.get(node).get(i);
+                inOrder[ele]--;
+                if(inOrder[ele]==0){
+                    que.add(ele);
+                }
+            }
+        }
+        return ans.size()==adj.size()?true:false;
+
+    }
+}
