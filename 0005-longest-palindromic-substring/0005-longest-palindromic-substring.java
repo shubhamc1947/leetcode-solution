736df248050aa4c3020b6f4c4ab1afa25415833f
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-03-15 22:10:42 +0530

    Time: 15 ms (86.64%), Space: 42.2 MB (89.67%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,27 +1,36 @@
 class Solution {
-    int maxLen = 0;
-    int lo = 0;
     public String longestPalindrome(String s) {
-        char[] input = s.toCharArray();
-        if(s.length() < 2) {
-            return s;
+        if (s == null || s.length() < 1) return "";
+        
+        int start = 0, end = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            // Get lengths for both odd and even length palindromes
+            int len1 = expandAroundCenter(s, i, i);     // Odd length palindrome
+            int len2 = expandAroundCenter(s, i, i + 1); // Even length palindrome
+            
+            // Get the maximum length
+            int len = Math.max(len1, len2);
+            
+            // Update start and end if we found a longer palindrome
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
         }
         
-        for(int i = 0; i<input.length; i++) {
-            expandPalindrome(input, i, i);
-            expandPalindrome(input, i, i+1);
-        }
-        return s.substring(lo, lo+maxLen);
+        return s.substring(start, end + 1);
     }
     
-    public void expandPalindrome(char[] s, int j, int k) {
-        while(j >= 0 && k < s.length && s[j] == s[k]) {
-            j--;
-            k++;
+    // Helper function to expand around center
+    private int expandAroundCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
         }
-        if(maxLen < k - j - 1) {
-            maxLen = k - j - 1;
-            lo = j+1;
-        }
+        
+        // Return the length of the palindrome
+        // (right - left - 1) is the length of the palindrome
+        return right - left - 1;
     }
 }
