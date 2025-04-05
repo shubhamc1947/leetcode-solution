7982c104894cf39e9e53a7339954b33e96e20b19
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-04-05 08:44:37 +0530

    Time: 0 ms (100%), Space: 42.2 MB (62.36%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -1,35 +1,20 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
+    int maxlevel = 0;
     public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> res = new ArrayList<>();
-        if (root == null) return res;
-        Queue<TreeNode> q = new LinkedList<>();
-        q.offer(root);
-
-        while(!q.isEmpty()){
-            int n = q.size();
-            for(int i = 0; i < n; i++){
-                TreeNode node = q.poll();
-                if(i == 0) res.add(node.val); // retrieve right most node 
-
-                if(node.right != null) q.offer(node.right); // add right 1st so we get the right most node of tree 1st (FIFO)
-                if(node.left != null) q.offer(node.left);
-            }
+        List<Integer> list  = new ArrayList<>();
+        right(root,1,list);
+        return list ;
+    }
+    void right(TreeNode root,int level,List<Integer> list){
+        if(root==null){
+            return ;
         }
-        return res;
+        if(maxlevel<level){
+            list.add(root.val);
+            maxlevel=level;
+        }
+        right(root.right,level+1,list);
+        right(root.left,level+1,list);
+        
     }
 }
