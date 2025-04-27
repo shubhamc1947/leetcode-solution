f2326e8de2656518c708c2daf30d71e6e34c9a8e
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-04-27 11:34:12 +0530

    Time: 0 ms (100%), Space: 41.8 MB (51.36%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,25 +1,24 @@
 class Solution {
-    public int f(int obs[][],int i,int j,int dp[][]){
-        if(i==0 && j==0) {
-            if(obs[i][j]==1) return 0;
-            return 1;
-        }
-        if(i<0 || j<0 || obs[i][j]==1) return 0;
-
-        if(dp[i][j]!=-1) return dp[i][j];
-        int left=f(obs,i,j-1,dp);
-        int top=f(obs,i-1,j,dp);
-        return dp[i][j] = left+top;
-    }
     public int uniquePathsWithObstacles(int[][] obs) {
         int n=obs.length;
         int m=obs[0].length;
-
         int dp[][]=new int[n][m];
         for(int i=0;i<n;i++){
-            Arrays.fill(dp[i],-1);
+            for(int j=0;j<m;j++){
+                if(obs[i][j]==1){
+                    dp[i][j]=0;
+                }else{//not 1 
+                    if(i==0 && j==0){
+                        dp[i][j]=1;
+                    }else{
+                        int right=0,down=0;
+                        if(j-1>=0) right=dp[i][j-1];
+                        if(i-1>=0) down=dp[i-1][j];
+                        dp[i][j]=right+down;
+                    }
+                }
+            }
         }
-
-        return f(obs,n-1,m-1,dp);
+        return dp[n-1][m-1];
     }
 }
