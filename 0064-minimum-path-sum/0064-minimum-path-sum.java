ce6bbe71a1b27513809450c2b4b2a5c1af16b7d9
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-04-27 12:22:28 +0530

    Time: 6 ms (12.96%), Space: 47.8 MB (33.24%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -12,9 +12,21 @@ class Solution {
         int n=arr.length;
         int m=arr[0].length;
         int dp[][]=new int[n][m];
+        
         for(int i=0;i<n;i++){
-            Arrays.fill(dp[i],-1);
+            for(int j=0;j<m;j++){
+                if(i==0 && j==0){
+                    dp[i][j]= arr[i][j];
+                }
+                else{
+                    int right=Integer.MAX_VALUE,down=Integer.MAX_VALUE;
+                    if(j-1>=0) right=dp[i][j-1]+arr[i][j];
+                    if(i-1>=0) down=dp[i-1][j]+arr[i][j];
+
+                    dp[i][j]=Math.min(right,down);
+                }
+            }
         }
-        return f(arr,n-1,m-1,dp);
+        return dp[n-1][m-1];
     }
 }
