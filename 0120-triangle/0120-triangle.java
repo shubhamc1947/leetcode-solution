7457eab051cab1895909df0679ff4278c195e2f0
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-04-27 20:50:13 +0530

    Time: 4 ms (45.05%), Space: 44.5 MB (36.77%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,23 +1,23 @@
 class Solution {
-    public int f(List<List<Integer>>arr,int i,int j,int n,int m,int dp[][] ){
-        if(i==n-1) return arr.get(i).get(j);
-
-        if(dp[i][j]!=-1) return dp[i][j];
-        int bottom =f(arr,i+1,j,n,m,dp)+arr.get(i).get(j);
-        int diag=f(arr,i+1,j+1,n,m,dp)+arr.get(i).get(j);
-
-        return dp[i][j]= Math.min(bottom,diag);
-
-    }
+    
     public int minimumTotal(List<List<Integer>> arr) {
         int n=arr.size();
         int m=arr.get(n-1).size();
 
         int dp[][]=new int[n][m];
-        for(int i=0;i<n;i++){
-            Arrays.fill(dp[i],-1);
-        }
         
-        return f(arr,0,0,n,m,dp);
+        //base case
+        for(int j=0;j<m;j++){
+            dp[n-1][j]=arr.get(n-1).get(j);
+        }
+
+        for(int i=n-2;i>=0;i--){
+            for(int j=i;j>=0;j--){
+                int bottom=dp[i+1][j]+arr.get(i).get(j);
+                int dia=dp[i+1][j+1]+arr.get(i).get(j);
+                dp[i][j]=Math.min(bottom,dia);
+            }
+        }
+        return dp[0][0];
     }
 }
