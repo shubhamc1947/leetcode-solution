b10de9c8a556c273df1af8448a2f5b2eed2ff2b3
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-04-27 12:22:35 +0530

    Time: 6 ms (12.96%), Space: 48 MB (15.15%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,13 +1,4 @@
 class Solution {
-    public int f(int arr[][],int i,int j,int dp[][]){
-        if(i==0 && j==0) return arr[0][0];
-        if(i<0 || j<0 ) return (int)1e9;
-        if(dp[i][j]!=-1) return dp[i][j];
-        int top=f(arr,i-1,j,dp)+arr[i][j];
-        int left=f(arr,i,j-1,dp)+arr[i][j];
-
-        return dp[i][j]= Math.min(top,left);
-    }
     public int minPathSum(int[][] arr) {
         int n=arr.length;
         int m=arr[0].length;
