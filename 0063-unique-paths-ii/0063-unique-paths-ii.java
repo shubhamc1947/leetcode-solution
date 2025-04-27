0d38d02bbfe63de748a5cdbbcf353f1994c91bfa
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-04-27 11:14:37 +0530

    Time: 0 ms (100%), Space: 42 MB (39.78%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,36 +1,25 @@
-class Solution
-{
-    int help(int[][]path,int n,int m,int dp[][])
-    {
-        if(n<0 || m<0)
-        {
-            return 0;
-        }
-        if(path[n][m]==1)
-        {
-            return 0;
-        }
-        if(n==0 && m==0)
-        {
+class Solution {
+    public int f(int obs[][],int i,int j,int dp[][]){
+        if(i==0 && j==0) {
+            if(obs[i][j]==1) return 0;
             return 1;
-            //basecase
         }
-        if(dp[n][m]!=-1)return dp[n][m];
-       
-     
-        int up=help(path,n-1,m,dp);
-        int left=help(path,n,m-1,dp);
-        return dp[n][m]=up+left;
+        if(i<0 || j<0 || obs[i][j]==1) return 0;
+
+        if(dp[i][j]!=-1) return dp[i][j];
+        int left=f(obs,i,j-1,dp);
+        int top=f(obs,i-1,j,dp);
+        return dp[i][j] = left+top;
     }
-    public int uniquePathsWithObstacles(int[][] path) 
-    {
-        int n=path.length;
-        int m=path[0].length;
-        int dp[][]=new int[n+1][m+1];
-        for(int []arr:dp)
-        {
-            Arrays.fill(arr,-1);
+    public int uniquePathsWithObstacles(int[][] obs) {
+        int n=obs.length;
+        int m=obs[0].length;
+
+        int dp[][]=new int[n][m];
+        for(int i=0;i<n;i++){
+            Arrays.fill(dp[i],-1);
         }
-        return help(path,n-1,m-1,dp);        
+
+        return f(obs,n-1,m-1,dp);
     }
 }
