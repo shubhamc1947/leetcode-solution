1bc7afd7a7ad2ece705b929daaabb02353e795e8
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-04-27 10:42:28 +0530

    Time: 0 ms (100%), Space: 40.2 MB (88.28%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,21 +1,17 @@
 class Solution {
-    public int uniquePaths(int m, int n) {
-        int[][] path=new int[m][n];
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                path[i][j]=-1;
-            }
-        }
-        return find(path, m-1, n-1);
+    public int f(int i,int j,int dp[][]){
+        if(i==0 && j==0) return 1;
+        if(i<0 || j<0) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int up=f(i-1,j,dp);
+        int left=f(i,j-1,dp);
+        return dp[i][j]=up+left;
     }
-    private int find(int[][] path, int m, int n){
-        //base cases
-        if(m==0 || n==0) return 1;
-        if(path[m][n] != -1) return path[m][n];
-
-        //recursive case
-        path[m][n]=find(path, m-1,n) + find(path, m, n-1);
-
-        return path[m][n];
+    public int uniquePaths(int m, int n) {
+        int dp[][]=new int[m][n];
+        for(int i=0;i<m;i++){
+            Arrays.fill(dp[i],-1);
+        }
+        return f(m-1,n-1,dp);
     }
 }
