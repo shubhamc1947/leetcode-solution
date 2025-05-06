a74157328c5a8d8885c80ac7ec937b7bb351a34d
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-05-06 08:21:06 +0530

    Time: 5 ms (31%), Space: 43.9 MB (89.09%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -4,22 +4,20 @@ class Solution {
         int n=arr.size();
         int m=arr.get(n-1).size();
 
-        int dp[]=new int[m];
+        int dp[][]=new int[n][m];
         
         //base case
         for(int j=0;j<m;j++){
-            dp[j]=arr.get(n-1).get(j);
+            dp[n-1][j]=arr.get(n-1).get(j);
         }
 
         for(int i=n-2;i>=0;i--){
-            int curr[]=new int[i+1];
             for(int j=i;j>=0;j--){
-                int bottom=dp[j]+arr.get(i).get(j);
-                int dia=dp[j+1]+arr.get(i).get(j);
-                curr[j]=Math.min(bottom,dia);
+                int bottom=dp[i+1][j]+arr.get(i).get(j);
+                int dia=dp[i+1][j+1]+arr.get(i).get(j);
+                dp[i][j]=Math.min(bottom,dia);
             }
-            dp=curr;
         }
-        return dp[0];
+        return dp[0][0];
     }
 }
