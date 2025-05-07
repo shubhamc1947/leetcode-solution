3173b1437be0944d2cbb936de7cbc99b89974530
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-05-07 22:55:32 +0530

    Time: 0 ms (100%), Space: 40.3 MB (65.81%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,19 +1,13 @@
 class Solution {
-    public int f(int i,int dp[]){
-        if(i<0){
-            return 0;
-        }
-        if(i==0 || i==1){
-            return 1;
-        }
-        if(dp[i]!=-1) return dp[i];
-        int oneStep=f(i-1,dp);
-        int twoStep=f(i-2,dp);
-        return dp[i]= oneStep+twoStep;
-    }
     public int climbStairs(int n) {
+        if(n<0) return 0;
         int dp[]=new int[n+1];
-        Arrays.fill(dp,-1);
-        return f(n,dp);
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        
+        return dp[n];
     }
 }
