771ddb7dd22bce82206d867b5430f5f8d479bc0a
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-05-11 23:03:14 +0530

    Time: 0 ms (100%), Space: 42.5 MB (25.81%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,21 +1,28 @@
 class Solution {
-    public void rotate(int[][] matrix) {
-        int n = matrix.length;
-        // Transpose the matrix
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[j][i];
-                matrix[j][i] = temp;
+    public void trans(int arr[][]){
+        int n=arr.length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                int temp=arr[i][j];
+                arr[i][j]=arr[j][i];
+                arr[j][i]=temp;
             }
         }
-        // Reverse each row
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n / 2; j++) {
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[i][n - 1 - j];
-                matrix[i][n - 1 - j] = temp;
+    }
+
+    public void rotate(int[][] arr) {
+        int n=arr.length;
+        trans(arr);
+        for(int i=0;i<n;i++){
+            int si=0,ei=n-1;
+            while(si<=ei){
+                int temp=arr[i][si];
+                arr[i][si]=arr[i][ei];
+                arr[i][ei]=temp;
+                si++;
+                ei--;
             }
         }
+
     }
 }
