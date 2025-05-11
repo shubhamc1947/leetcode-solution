f56543ccb92fc32eae9c23990fd723bd3a67d649
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-05-11 17:02:06 +0530

    Time: 0 ms (100%), Space: 41.5 MB (39.3%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.java
@@ -1,40 +1,20 @@
 class Solution {
-    public int minEleIdx(int arr[]){
-        int min=0;
-        for(int i=0;i<arr.length;i++){
-            if(arr[min]>=arr[i]){
-                min=i;
+
+// To check if the array is sorted and then rotated, I thought about how a sorted array behaves: all elements are in ascending order.
+// If it’s rotated, there might be one place where the order breaks (i.e., an element is greater than the next).
+// So, I counted how many such "breaks" occur in the circular array.
+    public boolean check(int[] nums) {
+        int n=nums.length;
+        int count=0;
+        for (int i=0;i<n;i++){
+            if(nums[i]>nums[(i+1)%n]){
+                count++;
             }
         }
-        return min;
-    }
-    public boolean testEqual(int arr[],int si,int ei){
-        int n=arr.length;
-        int small=arr[si];
-        System.out.println(si+" "+ei+" ");
-        for(int i=si;i<=ei;i++){
-            if(arr[i]!=small){
-                return false;
-            }
+        if (count<=1){
+            return true;}
+        else{
+            return false;
         }
-        return true;
-    }
-    public boolean check(int[] arr) {
-        int n=arr.length;
-        int minIdx=minEleIdx(arr);
-        System.out.println(minIdx);
-        int flag=arr[minIdx];
-        for(int i=minIdx;i<minIdx+n;i++){
-            if(flag<=arr[i%n]){
-                flag=arr[i%n];
-            }else{
-                if(arr[i%n]!=arr[minIdx]){
-                    return false;
-                }else{
-                    return testEqual(arr,i%n,minIdx);
-                }
-            }
-        }
-        return true;
     }
 }
