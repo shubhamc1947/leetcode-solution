f4dddc9c652f43c542995b6c8bf0f0c4238c7375
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-05-18 10:40:54 +0530

    Time: 5 ms (29.11%), Space: 47.6 MB (69.36%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,27 +1,22 @@
 class Solution {
-    public int func(int arr[][],int i,int j,int dp[][]){
-        //base cases
-        if(i==0 && j==0) return arr[i][j];
-
-        if(i<0 || j<0) return (int)1e7;
-
-        if(dp[i][j]!=-1) return dp[i][j];
-
-        int left=func(arr,i,j-1,dp)+arr[i][j];
-        int top=func(arr,i-1,j,dp)+arr[i][j];
-
-        return dp[i][j]= Math.min(left,top);
-    }
     public int minPathSum(int[][] arr) {
         int n=arr.length;
         int m=arr[0].length;
-
         int dp[][]=new int[n][m];
 
         for(int i=0;i<n;i++){
-            Arrays.fill(dp[i],-1);
+            for(int j=0;j<m;j++){
+                if(i==0 && j==0){
+                    dp[i][j]=arr[i][j];
+                }else{
+                    int left=(int)1e7;
+                    int top=(int)1e7;
+                    if(i>0) top=dp[i-1][j]+arr[i][j];
+                    if(j>0) left=dp[i][j-1]+arr[i][j];
+                    dp[i][j]=Math.min(left,top);
+                }
+            }
         }
-
-        return func(arr,n-1,m-1,dp);
+        return dp[n-1][m-1];
     }
 }
