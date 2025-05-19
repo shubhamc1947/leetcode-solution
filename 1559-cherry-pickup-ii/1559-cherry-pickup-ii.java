168cf38824bd8ba769831f41924532d6d9a549e6
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-05-19 13:19:28 +0530

    Time: 42 ms (13.12%), Space: 45.5 MB (87.25%) - LeetHub

--- 1559-cherry-pickup-ii/1559-cherry-pickup-ii.java
+++ 1559-cherry-pickup-ii/1559-cherry-pickup-ii.java
@@ -3,16 +3,17 @@ class Solution {
     public int cherryPickup(int[][] arr) {
         int n=arr.length;
         int m=arr[0].length;
-        int dp[][][]=new int[n][m][m];
+        int prev[][]=new int[m][m];
 
         for(int j1=0;j1<m;j1++){
             for(int j2=0;j2<m;j2++){
-                if(j1==j2) dp[n-1][j1][j2]=arr[n-1][j2];
-                else dp[n-1][j1][j2]=arr[n-1][j1]+arr[n-1][j2];
+                if(j1==j2) prev[j1][j2]=arr[n-1][j2];
+                else prev[j1][j2]=arr[n-1][j1]+arr[n-1][j2];
             }
         }
 
         for(int i=n-2;i>=0;i--){
+            int curr[][]=new int[m][m];
             for(int j1=0;j1<m;j1++){
                 for(int j2=0;j2<m;j2++){
                     int maxi=-(int)1e8;
@@ -21,7 +22,7 @@ class Solution {
                         for(int d2=-1;d2<=1;d2++){
                             int currVal=-(int)1e8;
                             if(j1+d1>=0 && j1+d1<m && j2+d2>=0 && j2+d2<m){
-                                currVal=dp[i+1][j1+d1][j2+d2];
+                                currVal=prev[j1+d1][j2+d2];
 
                             }
                             if(j1==j2){
@@ -32,11 +33,12 @@ class Solution {
                             maxi=Math.max(maxi,currVal);
                         }
                     }
-                    dp[i][j1][j2] = maxi;
+                    curr[j1][j2] = maxi;
                 }
             }
+            prev=curr;
         }
 
-        return dp[0][0][m-1];
+        return prev[0][m-1];
     }
 }
