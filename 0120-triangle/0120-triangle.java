fd2ec432d6441a6e02b502d848f2febedd366be5
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-05-19 07:58:33 +0530

    Time: 5 ms (30.3%), Space: 44.2 MB (66.35%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -7,7 +7,7 @@ class Solution {
             prev[j]=arr.get(n-1).get(j);
         }
         for(int i=n-2;i>=0;i--){
-            int curr[]=new int[n];
+            int curr[]=new int[i+1];
             for(int j=0;j<=i;j++){
                 int bottom=prev[j]+arr.get(i).get(j);
                 int dia=prev[j+1]+arr.get(i).get(j);
