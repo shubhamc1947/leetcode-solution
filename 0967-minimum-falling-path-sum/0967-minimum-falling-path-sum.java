e95464e7843b33aaab6096005a4f4dbbc1693720
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-05-19 08:48:19 +0530

    Time: 9 ms (20%), Space: 45.3 MB (77.28%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.java
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.java
@@ -2,31 +2,35 @@ class Solution {
     public int minFallingPathSum(int[][] arr) {
         int n=arr.length;
         int m=arr[0].length;
-        int ans=Integer.MAX_VALUE;
-        // int dp[][]=new int[n][m];
-        int dp[]=new int[m];
+        
+        int maxi=Integer.MAX_VALUE;
 
+        int dp[][]=new int[n][m];
+
+        //base case initilization
         for(int j=0;j<m;j++){
-            dp[j]=arr[n-1][j];
-        }
-        for(int i=n-2;i>=0;i--){
-            int currArr[]=new int[m];
-            for(int j=0;j<m;j++){
-                int mini=(int)1e9;
-                for(int k=-1;k<=1;k++){
-                    if(j+k>=0 && j+k <m){
-                        int curr=dp[j+k]+arr[i][j];
-                        mini=Math.min(mini,curr);
-                    }
-                }
-                currArr[j]=mini;
-            }
-            dp=currArr;
+            dp[0][j]=arr[0][j];
         }
 
-        for(int i=0;i<m;i++){
-            ans=Math.min(ans,dp[i]);
+        for(int i=1;i<n;i++){
+            for(int j=0;j<m;j++){
+                int left=(int)1e8;
+                int right=(int)1e8;
+                
+                int top=dp[i-1][j]+arr[i][j];
+                if(j-1>=0) left=dp[i-1][j-1]+arr[i][j];
+                if(j+1<m) right=dp[i-1][j+1]+arr[i][j];
+
+                dp[i][j]=Math.min(left,Math.min(right,top));
+            }
         }
-        return ans;
+
+
+        int mini=dp[n-1][0];
+        for(int j=1;j<m;j++){
+            mini=Math.min(mini,dp[n-1][j]);
+        }
+        return mini;
+        
     }
 }
