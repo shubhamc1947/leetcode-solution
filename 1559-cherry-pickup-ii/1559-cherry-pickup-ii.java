b3c39f9550eed037695dc490aeb9ec7fddf85888
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-05-19 12:59:03 +0530

    Time: 24 ms (78.54%), Space: 46.4 MB (31.02%) - LeetHub

--- 1559-cherry-pickup-ii/1559-cherry-pickup-ii.java
+++ 1559-cherry-pickup-ii/1559-cherry-pickup-ii.java
@@ -1,28 +1,25 @@
 class Solution {
-    public int f(int arr[][],int i,int j1,int j2,int n,int m,int dp[][][]){
-        if(j1<0 || j1>=m || j2<0 || j2>=m ){
-            return -(int)1e8;
-        }
+    public int func(int arr[][],int i,int j1,int j2, int n,int m,int dp[][][]){
+        if(j1<0 || j1>=m || j2<0 || j2>=m) return -(int)1e8;
         if(i==n-1){
-            if(j1==j2) return arr[i][j1];
-            else{
+            if(j1==j2){
+                return arr[i][j1];
+            }else{
                 return arr[i][j1]+arr[i][j2];
             }
         }
-        if(dp[i][j1][j2]!=-1){
-            return dp[i][j1][j2];
-        }
-        int maxi=Integer.MIN_VALUE;
-        for(int x=-1;x<=1;x++){
-            for(int y=-1;y<=1;y++){
-                int robo=f(arr,i+1,j1+x,j2+y,n,m,dp);
-                if(j1==j2){
-                    robo+=arr[i][j1];
-                }else{
-                    robo+=arr[i][j1]+arr[i][j2];
-                }
+        if(dp[i][j1][j2]!=-1) return dp[i][j1][j2];
 
-                maxi=Math.max(maxi,robo);
+        int maxi=-(int)1e8;
+        for(int d1=-1;d1<=1;d1++){
+            for(int d2=-1;d2<=1;d2++){
+                int currVal=func(arr,i+1,j1+d1,j2+d2,n,m,dp);
+                if(j1==j2){
+                    currVal+=arr[i][j1];
+                }else{
+                    currVal+=arr[i][j1]+arr[i][j2];
+                }
+                maxi=Math.max(maxi,currVal);
             }
         }
         return dp[i][j1][j2] = maxi;
@@ -30,14 +27,12 @@ class Solution {
     public int cherryPickup(int[][] arr) {
         int n=arr.length;
         int m=arr[0].length;
-        int dp[][][]=new int [n][m][m];
+        int dp[][][]=new int[n][m][m];
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                for(int k=0;k<m;k++){
-                    dp[i][j][k]=-1;
-                }
+                Arrays.fill(dp[i][j],-1);
             }
         }
-        return f(arr,0,0,m-1,n,m,dp);
+        return func(arr,0,0,m-1,n,m,dp);
     }
 }
