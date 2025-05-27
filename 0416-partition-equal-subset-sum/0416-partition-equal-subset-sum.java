9dd0a4b3f1f1413cb340f2fd199aec23b25907f1
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-05-27 22:33:43 +0530

    Time: 100 ms (14.08%), Space: 57.3 MB (20.79%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,32 +1,37 @@
 class Solution {
     public boolean func(int arr[],int i,int tar,int dp[][]){
         if(tar==0) return true;
-        if(i<0) return false;
+        if(i==0) return arr[i]==tar;
+
         if(dp[i][tar]!=-1) return dp[i][tar]==1;
         boolean notTake=func(arr,i-1,tar,dp);
         boolean take=false;
         if(tar>=arr[i]){
+
             take=func(arr,i-1,tar-arr[i],dp);
         }
-
-        dp[i][tar]= take|| notTake?1:0;
+        dp[i][tar]=take||notTake?1:0;
         return take||notTake;
-
     }
     public boolean canPartition(int[] arr) {
-        int totalSum=0;
         int n=arr.length;
+
+        if(n==1) return false;
+
+        int totSum=0;
         for(int i=0;i<n;i++){
-            totalSum+=arr[i];
+            totSum+=arr[i];
         }
-        if(totalSum%2!=0){
-            return false;
-        }
-        int tar=totalSum/2;
-        int dp[][]=new int[n][tar+1];
+
+        if(totSum%2!=0) return false;
+
+        int target=totSum/2;
+        int dp[][]=new int[n][target+1];
         for(int i=0;i<n;i++){
             Arrays.fill(dp[i],-1);
         }
-        return func(arr,n-1,tar,dp);
+
+        return func(arr,n-1,target,dp);
+
     }
 }
