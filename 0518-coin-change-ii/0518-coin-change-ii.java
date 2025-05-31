c650e973050fcd9149aaca93df874ec1adbfa0a0
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-05-31 06:37:17 +0530

    Time: 9 ms (62.85%), Space: 49 MB (53.37%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,23 +1,4 @@
 class Solution {
-    // public int func(int arr[],int tar,int idx,int dp[][]){
-    //     if(idx==0){
-    //         if(tar%arr[0]==0){
-    //             return 1;
-    //         }else{
-    //             return 0;
-    //         }
-    //     }
-    //     if(dp[idx][tar]!=-1) return dp[idx][tar];
-
-    //     //not Take
-    //     int notTake=0+func(arr,tar,idx-1,dp);
-
-    //     int take=0;
-    //     if(tar>=arr[idx]){
-    //         take=func(arr,tar-arr[idx],idx,dp);
-    //     }
-    //     return dp[idx][tar]= take+notTake;
-    // }
     public int change(int t, int[] arr) {
         int n=arr.length;
         int dp[][]=new int[n][t+1];
