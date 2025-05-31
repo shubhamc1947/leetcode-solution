b6553925e3895850636d5f39d7ccb8f67215e609
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-05-31 06:37:01 +0530

    Time: 9 ms (62.85%), Space: 49.5 MB (23.41%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,29 +1,41 @@
 class Solution {
-    public int func(int arr[],int tar,int idx,int dp[][]){
-        if(idx==0){
-            if(tar%arr[0]==0){
-                return 1;
-            }else{
-                return 0;
+    // public int func(int arr[],int tar,int idx,int dp[][]){
+    //     if(idx==0){
+    //         if(tar%arr[0]==0){
+    //             return 1;
+    //         }else{
+    //             return 0;
+    //         }
+    //     }
+    //     if(dp[idx][tar]!=-1) return dp[idx][tar];
+
+    //     //not Take
+    //     int notTake=0+func(arr,tar,idx-1,dp);
+
+    //     int take=0;
+    //     if(tar>=arr[idx]){
+    //         take=func(arr,tar-arr[idx],idx,dp);
+    //     }
+    //     return dp[idx][tar]= take+notTake;
+    // }
+    public int change(int t, int[] arr) {
+        int n=arr.length;
+        int dp[][]=new int[n][t+1];
+
+        for(int tar=0;tar<=t;tar++){
+            if(tar % arr[0]==0) dp[0][tar]=1;
+        }
+
+        for(int idx=1;idx<n;idx++){
+            for(int tar=0;tar<=t;tar++){
+                int notTake=0+dp[idx-1][tar];
+                int take=0;
+                if(tar>=arr[idx]){
+                    take=dp[idx][tar-arr[idx]];
+                }
+                dp[idx][tar]= take+notTake;
             }
         }
-        if(dp[idx][tar]!=-1) return dp[idx][tar];
-
-        //not Take
-        int notTake=0+func(arr,tar,idx-1,dp);
-
-        int take=0;
-        if(tar>=arr[idx]){
-            take=func(arr,tar-arr[idx],idx,dp);
-        }
-        return dp[idx][tar]= take+notTake;
-    }
-    public int change(int tar, int[] arr) {
-        int n=arr.length;
-        int dp[][]=new int[n][tar+1];
-        for(int i=0;i<n;i++){
-            Arrays.fill(dp[i],-1);
-        }
-        return func(arr,tar,n-1,dp);
+        return dp[n-1][t];
     }
 }
