e2aeff63ff0e24f0e2170522cfbf75f3f65a2860
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-06-02 01:42:18 +0530

    Time: 21 ms (85.32%), Space: 50.4 MB (74.91%) - LeetHub

--- 1170-shortest-common-supersequence/1170-shortest-common-supersequence.java
+++ 1170-shortest-common-supersequence/1170-shortest-common-supersequence.java
@@ -1,48 +1,48 @@
 class Solution {
-    
-    public String shortestCommonSupersequence(String str1, String str2) {
+    public String func(String s1, String s2) {
+        // code here
+        int n1=s1.length();
+        int n2=s2.length();
+        int dp[][]=new int[n1+1][n2+1];
 
-        char[] s = str1.toCharArray(), t=str2.toCharArray();
-        int n=str1.length(), m=str2.length();
-
-        int dp[][] = new int[n+1][m+1];
-        for(int i=0; i<=n; i++) dp[i][0]=0;
-        for(int j=0; j<=m; j++) dp[0][j]=0;
-
-        for(int i=1; i<=n; i++)
-        {
-            for(int j=1; j<=m; j++)
-            {
-                if(s[i-1]==t[j-1])
-                dp[i][j]=1+dp[i-1][j-1];
-                
-                else 
-                dp[i][j]=Math.max(dp[i-1][j], dp[i][j-1]);
+        for(int i=1;i<=n1;i++){
+            for(int j=1;j<=n2;j++){
+                if(s1.charAt(i-1)==s2.charAt(j-1)){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }else{
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+                }
             }
         }
-        int i=n,  j=m;
-        StringBuilder sb = new StringBuilder();
-        while(i>0 && j>0)
-        {
-            if(s[i-1]==t[j-1]){
-                sb.append(s[i-1]);
-                i--; j--;
-            }
-            else if(dp[i-1][j] <= dp[i][j-1]){
-                sb.append(t[j-1]);
+
+        StringBuilder ans=new StringBuilder();
+        int i=n1,j=n2;
+        while(i>0 && j>0){
+            if(s1.charAt(i-1)==s2.charAt(j-1)){
+                ans.append(s1.charAt(i-1));
+                i--;
+                j--;
+            }else if( dp[i-1][j]>dp[i][j-1]){
+                ans.append(s1.charAt(i-1));
+                i--;
+            }else{
+                ans.append(s2.charAt(j-1));
                 j--;
             }
-            // up cell is greater we will move up and will add char s[i-1] to ans.
-            else{
-                sb.append(s[i-1]);
-                i--;
-            }
         }
-
-        //adding remaining character of both string.
-        while(i-->0) sb.append(s[i]);
-        while(j-->0) sb.append(t[j]);
+        while(i>0){
+            ans.append(s1.charAt(i-1));
+            i--;
+        }
+        while(j>0){
+            ans.append(s2.charAt(j-1));
+            j--;
+        }
+        return ans.reverse().toString();
         
-        return sb.reverse().toString();
+    }
+    public String shortestCommonSupersequence(String s1, String s2) {
+        
+        return func(s1,s2);
     }
 }
