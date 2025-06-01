e638d8deae242cfacf616c4b71e0cdd9855fb132
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-06-02 01:43:27 +0530

    Time: 21 ms (85.32%), Space: 50.3 MB (83.2%) - LeetHub

--- 1170-shortest-common-supersequence/1170-shortest-common-supersequence.java
+++ 1170-shortest-common-supersequence/1170-shortest-common-supersequence.java
@@ -4,7 +4,7 @@ class Solution {
         int n1=s1.length();
         int n2=s2.length();
         int dp[][]=new int[n1+1][n2+1];
-
+        //creating the dp of LCS 
         for(int i=1;i<=n1;i++){
             for(int j=1;j<=n2;j++){
                 if(s1.charAt(i-1)==s2.charAt(j-1)){
@@ -14,7 +14,7 @@ class Solution {
                 }
             }
         }
-
+        //Creating the shortestCommonSupersequence
         StringBuilder ans=new StringBuilder();
         int i=n1,j=n2;
         while(i>0 && j>0){
@@ -30,6 +30,8 @@ class Solution {
                 j--;
             }
         }
+
+        //covering the left String 
         while(i>0){
             ans.append(s1.charAt(i-1));
             i--;
@@ -38,6 +40,7 @@ class Solution {
             ans.append(s2.charAt(j-1));
             j--;
         }
+        //reverse String is the Answer
         return ans.reverse().toString();
         
     }
