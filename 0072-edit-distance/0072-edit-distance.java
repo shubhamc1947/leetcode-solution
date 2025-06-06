5fdaced7cf19b53175cb32b0752a683a4dee0f58
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-06-06 06:39:14 +0530

    Time: 3 ms (94.61%), Space: 45.1 MB (46.33%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,10 +1,10 @@
 class Solution {
     public int func(int i,int j,String s1, String s2, int dp[][]){
-        if(i<0) return j+1;
-        if(j<0) return i+1;
+        if(i==0) return j;
+        if(j==0) return i;
         //match
         if(dp[i][j]!=-1) return dp[i][j];
-        if(s1.charAt(i)==s2.charAt(j)){
+        if(s1.charAt(i-1)==s2.charAt(j-1)){
             //match
             return dp[i][j]= func(i-1,j-1,s1,s2,dp);
         }else{
@@ -20,10 +20,10 @@ class Solution {
     public int minDistance(String s1, String s2) {
         int n=s1.length();
         int m=s2.length();
-        int dp[][]=new int[n][m];
-        for(int i=0;i<n;i++){
+        int dp[][]=new int[n+1][m+1];
+        for(int i=0;i<=n;i++){
             Arrays.fill(dp[i],-1);
         }
-        return func(n-1,m-1,s1,s2,dp);
+        return func(n,m,s1,s2,dp);
     }
 }
