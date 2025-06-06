9f48454a867f9c11b8b606f5a67b55c82d50b846
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-06-06 06:37:15 +0530

    Time: 2 ms (100%), Space: 44.9 MB (77.69%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,40 +1,29 @@
 class Solution {
-    private int solve(int i, int j, String s1, String s2, int dp[][]) {
-        if (i == 0 && j == 0) {
-            if (s1.charAt(i) == s2.charAt(j))
-                return 0;
-            else
-                return 1;
+    public int func(int i,int j,String s1, String s2, int dp[][]){
+        if(i<0) return j+1;
+        if(j<0) return i+1;
+        //match
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(s1.charAt(i)==s2.charAt(j)){
+            //match
+            return dp[i][j]= func(i-1,j-1,s1,s2,dp);
+        }else{
+            // insert
+            int insert=1+func(i,j-1,s1,s2,dp);
+            //delete
+            int delete=1+func(i-1,j,s1,s2,dp);
+            //replace
+            int replace=1+func(i-1,j-1,s1,s2,dp);
+            return dp[i][j]= Math.min(insert,Math.min(delete,replace));
         }
-        if (j == -1)
-            return i + 1;
-        if (i == -1)
-            return j + 1;
-
-        if (dp[i][j] != -1)
-            return dp[i][j];
-
-        int insert = 300000000;
-        int replace = 30000000;
-        int delete = 30000000;
-
-        if (s1.charAt(i) == s2.charAt(j)) 
-            return dp[i][j] = solve(i - 1, j - 1, s1, s2, dp);
-        else {
-            insert = 1 + solve(i, j - 1, s1, s2, dp);
-            replace = 1 + solve(i - 1, j - 1, s1, s2, dp);
-            delete = 1 + solve(i - 1, j, s1, s2, dp);
-        }
-
-        return dp[i][j] = Math.min(insert, Math.min(replace, delete));
     }
-
-    public int minDistance(String word1, String word2) {
-        int dp[][] = new int[word1.length()][word2.length()];
-
-        for (int[] I : dp)
-            Arrays.fill(I, -1);
-
-        return solve(word1.length() - 1, word2.length() - 1, word1, word2, dp);
+    public int minDistance(String s1, String s2) {
+        int n=s1.length();
+        int m=s2.length();
+        int dp[][]=new int[n][m];
+        for(int i=0;i<n;i++){
+            Arrays.fill(dp[i],-1);
+        }
+        return func(n-1,m-1,s1,s2,dp);
     }
 }
