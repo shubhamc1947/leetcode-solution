3fcff09d97c60e8e042606a15746694ba2ba9e60
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-06-06 07:23:32 +0530

    Time: 20 ms (50.49%), Space: 48.7 MB (22.6%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,21 +1,21 @@
 class Solution {
     public boolean func(String s1,String s2, int i,int j,int dp[][]){
-        if(i<0 && j<0) return true;
-        if(i<0 && j>=0) return false;
-        if(j<0 && i>=0) {
-            for(int x=0;x<=i;x++){
-                if(s1.charAt(x)!='*') return false;
+        if(i==0 && j==0) return true;
+        if(i==0 && j>=1) return false;
+        if(j==0 && i>=1) {
+            for(int x=1;x<=i;x++){
+                if(s1.charAt(x-1)!='*') return false;
             }
             return true;
         }
         if(dp[i][j]!=-1) return dp[i][j]==1;
 
         // match
-        if(s1.charAt(i)==s2.charAt(j) || s1.charAt(i)=='?'){
+        if(s1.charAt(i-1)==s2.charAt(j-1) || s1.charAt(i-1)=='?'){
             dp[i][j] = func(s1,s2,i-1,j-1,dp)?1:0;
             return dp[i][j]==1;
         }else{
-            if(s1.charAt(i)=='*'){
+            if(s1.charAt(i-1)=='*'){
                 //take * as ""
                 boolean notTake=func(s1,s2,i-1,j,dp);
                 // take * as curr char (but * can be another as well so taking j-1 only)
@@ -33,10 +33,10 @@ class Solution {
         //assuming s1 will contains * and ? 
         int n=s1.length();
         int m=s2.length();
-        int dp[][]=new int[n][m];
-        for(int i=0;i<n;i++){
+        int dp[][]=new int[n+1][m+1];
+        for(int i=0;i<=n;i++){
             Arrays.fill(dp[i],-1);
         }
-        return func(s1,s2,n-1,m-1,dp);
+        return func(s1,s2,n,m,dp);
     }
 }
