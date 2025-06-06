4527ebcb2f3825aed7e759400dc160d2ece40428
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-06-06 07:29:23 +0530

    Time: 25 ms (19.4%), Space: 44.7 MB (77.75%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,42 +1,46 @@
 class Solution {
-    public boolean func(String s1,String s2, int i,int j,int dp[][]){
-        if(i==0 && j==0) return true;
-        if(i==0 && j>=1) return false;
-        if(j==0 && i>=1) {
-            for(int x=1;x<=i;x++){
-                if(s1.charAt(x-1)!='*') return false;
-            }
-            return true;
-        }
-        if(dp[i][j]!=-1) return dp[i][j]==1;
 
-        // match
-        if(s1.charAt(i-1)==s2.charAt(j-1) || s1.charAt(i-1)=='?'){
-            dp[i][j] = func(s1,s2,i-1,j-1,dp)?1:0;
-            return dp[i][j]==1;
-        }else{
-            if(s1.charAt(i-1)=='*'){
-                //take * as ""
-                boolean notTake=func(s1,s2,i-1,j,dp);
-                // take * as curr char (but * can be another as well so taking j-1 only)
-                boolean take=func(s1,s2,i,j-1,dp);
-                dp[i][j] = take || notTake?1:0;
-                return take||notTake;
-            }else{
-                dp[i][j] = 0;
-                return false;
-            }
-        }
-        
-    }
     public boolean isMatch(String s2, String s1) {
         //assuming s1 will contains * and ? 
         int n=s1.length();
         int m=s2.length();
-        int dp[][]=new int[n+1][m+1];
-        for(int i=0;i<=n;i++){
-            Arrays.fill(dp[i],-1);
+        boolean dp[][]=new boolean[n+1][m+1];
+        
+        dp[0][0]=true;
+        for(int j=1;j<=m;j++){
+            dp[0][j]=false;
         }
-        return func(s1,s2,n,m,dp);
+        for(int i=1;i<=n;i++){
+            boolean flag=false;
+            for(int x=1;x<=i;x++){
+                if(s1.charAt(x-1)!='*'){
+                     flag=true;
+                     break;
+                }
+            }
+            if(!flag){
+                dp[i][0]=true;
+            }
+        }
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s1.charAt(i-1)==s2.charAt(j-1) || s1.charAt(i-1)=='?'){
+                    dp[i][j] = dp[i-1][j-1];
+                }else{
+                    if(s1.charAt(i-1)=='*'){
+                        //take * as ""
+                        boolean notTake=dp[i-1][j];
+                        // take * as curr char (but * can be another as well so taking j-1 only)
+                        boolean take=dp[i][j-1];
+                        dp[i][j] = take || notTake?true:false;
+                    }else{
+                        dp[i][j] = false;
+                    }
+                }
+            }
+        }
+
+        return dp[n][m];
     }
 }
