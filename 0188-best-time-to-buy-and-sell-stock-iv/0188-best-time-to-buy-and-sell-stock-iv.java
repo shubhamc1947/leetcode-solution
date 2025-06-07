b75e6174248c7cd53762f11bf53bd91f6b57c5f3
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-06-07 19:08:17 +0530

    Time: 8 ms (34.72%), Space: 45.2 MB (26.58%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,22 +1,25 @@
 class Solution {
-    public int maxProfit(int k, int[] prices) {
-        if (k == 0) {
-            return 0;
+    public int func(int arr[],int idx,int buy,int cap,int n,int dp[][][]){
+        if(idx==n || cap==0) return 0;
+        if(dp[idx][buy][cap]!=-1) return dp[idx][buy][cap];
+        if(buy==1){//we can sell
+            int currSell=arr[idx]+func(arr,idx+1,0,cap-1,n,dp);
+            int notSell=0+func(arr,idx+1,1,cap,n,dp);
+            return dp[idx][buy][cap]= Math.max(currSell,notSell);
+        }else{//buy =0
+            int currBuy=-arr[idx]+func(arr,idx+1,1,cap,n,dp);
+            int notBuy =func(arr,idx+1,0,cap,n,dp);
+            return dp[idx][buy][cap]= Math.max(currBuy,notBuy);
         }
-        
-        int[] buy = new int[k]; // buy[i]: min cost at (i + 1)-th time stock purchased
-        int[] sell = new int[k]; // sell[i]: max revenue at (i + 1)-th time stock sold
-        Arrays.fill(buy, Integer.MIN_VALUE);
-        Arrays.fill(sell, 0);
-        
-        for (int i : prices) {
-            buy[0] = Math.max(buy[0], i * -1);
-            sell[0] = Math.max(sell[0], buy[0] + i);
-            for (int j = 1; j < k; j++) { // The i-th time buy & sell depens on the (i - 1)-th time buy & sell
-                buy[j] = Math.max(buy[j], sell[j - 1] - i);
-                sell[j] = Math.max(sell[j], buy[j] + i);
+    }
+    public int maxProfit(int k, int[] arr) {
+        int n=arr.length;
+        int dp[][][]=new int[n][2][k+1];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<2;j++){
+                Arrays.fill(dp[i][j],-1);
             }
         }
-        return sell[k - 1];
+        return func(arr,0,0,k,n,dp);
     }
 }
