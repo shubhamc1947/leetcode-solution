6b9200a092ffa03d8fb7b12f66518be7f5aa003c
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-06-07 18:54:42 +0530

    Time: 1 ms (81.17%), Space: 41.6 MB (79.9%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -3,18 +3,13 @@ class Solution {
         int n=arr.length;
         int dp[][]=new int[n+2][2];
         for(int idx=n-1;idx>=0;idx--){
-            for(int buy=0;buy<2;buy++){
-                if(buy==1){ //sell
-                    int currSell=arr[idx]+dp[idx+2][0];
-                    int notSell=0+dp[idx+1][1];
-                    dp[idx][buy]= Math.max(currSell,notSell);
-                }else{//buy
-                    // System.out.println(idx);
-                    int currBuy=-arr[idx]+dp[idx+1][1];
-                    int notBuy=0+dp[idx+1][0];
-                    dp[idx][buy]= Math.max(currBuy,notBuy);
-                }
-            }
+            int currSell=arr[idx]+dp[idx+2][0];
+            int notSell=0+dp[idx+1][1];
+            dp[idx][1]= Math.max(currSell,notSell);
+            // System.out.println(idx);
+            int currBuy=-arr[idx]+dp[idx+1][1];
+            int notBuy=0+dp[idx+1][0];
+            dp[idx][0]= Math.max(currBuy,notBuy);
         }
         
         return dp[0][0];
