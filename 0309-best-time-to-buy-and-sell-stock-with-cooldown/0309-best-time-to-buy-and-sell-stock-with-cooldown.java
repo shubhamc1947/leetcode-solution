99a77890bec3cb4990cf6152be808cb2eeb8518e
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-06-07 18:52:44 +0530

    Time: 1 ms (81.17%), Space: 42 MB (33.74%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,25 +1,37 @@
 class Solution {
-    public int func(int arr[],int idx,int buy,int n,int dp[][]){
-        if(idx>=n) return 0;
-        if(dp[idx][buy]!=-1) return dp[idx][buy];
-        if(buy==1){ //sell
-            int currSell=arr[idx]+func(arr,idx+2,0,n,dp);
-            int notSell=0+func(arr,idx+1,1,n,dp);
-            return dp[idx][buy]= Math.max(currSell,notSell);
-        }else{//buy
-            // System.out.println(idx);
-            int currBuy=-arr[idx]+func(arr,idx+1,1,n,dp);
-            int notBuy=0+func(arr,idx+1,0,n,dp);
-            return dp[idx][buy]= Math.max(currBuy,notBuy);
-        }
-    }
+    // public int func(int arr[],int idx,int buy,int n,int dp[][]){
+    //     if(idx>=n) return 0;
+    //     if(dp[idx][buy]!=-1) return dp[idx][buy];
+    //     if(buy==1){ //sell
+    //         int currSell=arr[idx]+func(arr,idx+2,0,n,dp);
+    //         int notSell=0+func(arr,idx+1,1,n,dp);
+    //         return dp[idx][buy]= Math.max(currSell,notSell);
+    //     }else{//buy
+    //         // System.out.println(idx);
+    //         int currBuy=-arr[idx]+func(arr,idx+1,1,n,dp);
+    //         int notBuy=0+func(arr,idx+1,0,n,dp);
+    //         return dp[idx][buy]= Math.max(currBuy,notBuy);
+    //     }
+    // }
     public int maxProfit(int[] arr) {
         int n=arr.length;
-        int dp[][]=new int[n][2];
-        for(int i=0;i<n;i++){
-            Arrays.fill(dp[i],-1);
+        int dp[][]=new int[n+2][2];
+        for(int idx=n-1;idx>=0;idx--){
+            for(int buy=0;buy<2;buy++){
+                if(buy==1){ //sell
+                    int currSell=arr[idx]+dp[idx+2][0];
+                    int notSell=0+dp[idx+1][1];
+                    dp[idx][buy]= Math.max(currSell,notSell);
+                }else{//buy
+                    // System.out.println(idx);
+                    int currBuy=-arr[idx]+dp[idx+1][1];
+                    int notBuy=0+dp[idx+1][0];
+                    dp[idx][buy]= Math.max(currBuy,notBuy);
+                }
+            }
         }
-        return func(arr,0,0,arr.length,dp);
+        
+        return dp[0][0];
 
     }
 }
