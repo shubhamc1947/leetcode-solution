8d02475e319d9070bd252ff7556bd572856a391c
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-06-07 12:58:44 +0530

    Time: 56 ms (45.85%), Space: 64.2 MB (33.38%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -16,13 +16,41 @@ class Solution {
     }
     public int maxProfit(int[] arr) {    
         int n=arr.length;
-        int dp[][][]=new int[n][2][3];// buy=>0,1 , cap=>2,1,0
-        for(int i=0;i<n;i++){
-            for(int j=0;j<2;j++){
-                Arrays.fill(dp[i][j],-1);
+        int dp[][][]=new int[n+1][2][3];// buy=>0,1 , cap=>2,1,0
+        
+        //base i==n ? j=0,1  k=2,1,0
+        // dp[n][0][2]=0;
+        // dp[n][1][2]=0;
+        // dp[n][0][1]=0;
+        // dp[n][1][1]=0;
+        // dp[n][0][0]=0;
+        // dp[n][1][0]=0;
+
+        // //base cap=0 ? i=0-> <=n and j=0,1 
+        // for(int i=0;i<=n;i++){
+        //     for(int j=0;j<2;j++){
+        //         dp[i][j][0]=0;
+        //     }
+        // }
+
+        for(int i=n-1;i>=0;i--){
+            for(int buy=0;buy<2;buy++){
+                for(int cap=1;cap<3;cap++){//tricky as cap =0 return 0 so run for 1
+                    if(buy==1){ // we can sell
+                        int currSell=arr[i]+dp[i+1][0][cap-1];
+                        int notSell=dp[i+1][1][cap];
+                        dp[i][buy][cap]= Math.max(currSell,notSell);
+                    }else{ // we can buy buy=0
+                        int currBuy=-arr[i]+ dp[i+1][1][cap];
+                        int notBuy= dp[i+1][0][cap];
+                        dp[i][buy][cap]= Math.max(currBuy,notBuy);
+                    }
+                }
             }
         }
-        return func(arr,0,0,2,n,dp);
+
+
+        return dp[0][0][2];
 
     }
 }
