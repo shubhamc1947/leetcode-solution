072fda4e2a9449a958fd8ef0c140d3fd780e7349
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-06-07 19:26:06 +0530

    Time: 2 ms (80.8%), Space: 44.7 MB (41.97%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,36 +1,26 @@
 class Solution {
-    public int func(int arr[],int idx,int buy,int cap,int n,int dp[][][]){
-        if(idx==n || cap==0) return 0;
-        if(dp[idx][buy][cap]!=-1) return dp[idx][buy][cap];
-        if(buy==1){//we can sell
-            int currSell=arr[idx]+func(arr,idx+1,0,cap-1,n,dp);
-            int notSell=0+func(arr,idx+1,1,cap,n,dp);
-            return dp[idx][buy][cap]= Math.max(currSell,notSell);
-        }else{//buy =0
-            int currBuy=-arr[idx]+func(arr,idx+1,1,cap,n,dp);
-            int notBuy =func(arr,idx+1,0,cap,n,dp);
-            return dp[idx][buy][cap]= Math.max(currBuy,notBuy);
-        }
-    }
+
     public int maxProfit(int k, int[] arr) {
         int n=arr.length;
-        int dp[][][]=new int[n+1][2][k+1];
+        int next[][]=new int[2][k+1];
         for(int idx=n-1;idx>=0;idx--){
+            int curr[][]=new int[2][k+1];
             for(int buy=0;buy<2;buy++){
                 for(int cap=k;cap>0;cap--){
                     if(buy==1){//we can sell
-                        int currSell=arr[idx]+dp[idx+1][0][cap-1];
-                        int notSell=0+ dp[idx+1][1][cap];
-                        dp[idx][buy][cap]= Math.max(currSell,notSell);
+                        int currSell=arr[idx]+next[0][cap-1];
+                        int notSell=0+ next[1][cap];
+                        curr[buy][cap]= Math.max(currSell,notSell);
                     }else{//buy =0
                         // System.out.println(cap);
-                        int currBuy=-arr[idx]+dp[idx+1][1][cap];
-                        int notBuy =dp[idx+1][0][cap];
-                        dp[idx][buy][cap]= Math.max(currBuy,notBuy);
+                        int currBuy=-arr[idx]+next[1][cap];
+                        int notBuy =next[0][cap];
+                        curr[buy][cap]= Math.max(currBuy,notBuy);
                     }
                 }
             }
+            next=curr;
         }
-        return dp[0][0][k];
+        return next[0][k];
     }
 }
