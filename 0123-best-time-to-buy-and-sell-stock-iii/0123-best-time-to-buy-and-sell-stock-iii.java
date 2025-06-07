7e3bdeafd9b59d688970c097f220ffd7ad300a35
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-06-07 13:01:03 +0530

    Time: 64 ms (36.02%), Space: 58.5 MB (94.42%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -16,7 +16,7 @@ class Solution {
     }
     public int maxProfit(int[] arr) {    
         int n=arr.length;
-        int dp[][][]=new int[n+1][2][3];// buy=>0,1 , cap=>2,1,0
+        int ahead[][]=new int[2][3];// buy=>0,1 , cap=>2,1,0
         
         //base i==n ? j=0,1  k=2,1,0
         // dp[n][0][2]=0;
@@ -34,23 +34,25 @@ class Solution {
         // }
 
         for(int i=n-1;i>=0;i--){
+            int curr[][]=new int[2][3];
             for(int buy=0;buy<2;buy++){
                 for(int cap=1;cap<3;cap++){//tricky as cap =0 return 0 so run for 1
                     if(buy==1){ // we can sell
-                        int currSell=arr[i]+dp[i+1][0][cap-1];
-                        int notSell=dp[i+1][1][cap];
-                        dp[i][buy][cap]= Math.max(currSell,notSell);
+                        int currSell=arr[i]+ahead[0][cap-1];
+                        int notSell=ahead[1][cap];
+                        curr[buy][cap]= Math.max(currSell,notSell);
                     }else{ // we can buy buy=0
-                        int currBuy=-arr[i]+ dp[i+1][1][cap];
-                        int notBuy= dp[i+1][0][cap];
-                        dp[i][buy][cap]= Math.max(currBuy,notBuy);
+                        int currBuy=-arr[i]+ ahead[1][cap];
+                        int notBuy= ahead[0][cap];
+                        curr[buy][cap]= Math.max(currBuy,notBuy);
                     }
                 }
             }
+            ahead=curr;
         }
 
 
-        return dp[0][0][2];
+        return ahead[0][2];
 
     }
 }
