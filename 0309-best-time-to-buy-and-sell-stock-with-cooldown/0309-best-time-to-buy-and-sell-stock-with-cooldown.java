d227306dd888abd410412a6555c535af492deb94
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-06-07 18:52:53 +0530

    Time: 1 ms (81.17%), Space: 42.3 MB (12.84%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,18 +1,4 @@
 class Solution {
-    // public int func(int arr[],int idx,int buy,int n,int dp[][]){
-    //     if(idx>=n) return 0;
-    //     if(dp[idx][buy]!=-1) return dp[idx][buy];
-    //     if(buy==1){ //sell
-    //         int currSell=arr[idx]+func(arr,idx+2,0,n,dp);
-    //         int notSell=0+func(arr,idx+1,1,n,dp);
-    //         return dp[idx][buy]= Math.max(currSell,notSell);
-    //     }else{//buy
-    //         // System.out.println(idx);
-    //         int currBuy=-arr[idx]+func(arr,idx+1,1,n,dp);
-    //         int notBuy=0+func(arr,idx+1,0,n,dp);
-    //         return dp[idx][buy]= Math.max(currBuy,notBuy);
-    //     }
-    // }
     public int maxProfit(int[] arr) {
         int n=arr.length;
         int dp[][]=new int[n+2][2];
