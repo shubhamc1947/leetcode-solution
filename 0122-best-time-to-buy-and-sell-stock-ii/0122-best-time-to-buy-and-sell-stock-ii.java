0fa07ca1cb85a1483c852c236681670fbf5b5002
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-06-07 11:56:29 +0530

    Time: 1 ms (79.27%), Space: 45.9 MB (58.94%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,26 +1,27 @@
 class Solution {
-    public int func(int arr[],int idx,int buy, int n, int dp[][]){
-        if(idx==n) return 0;
-        if(dp[idx][buy]!=-1) return dp[idx][buy];
-        if(buy==1){
-            //either sell here 
-            int currSell=arr[idx]+func(arr,idx+1,0,n,dp);
-            // or don't sell here
-            int notSell=func(arr,idx+1,1,n,dp);
-            return dp[idx][buy]= Math.max(currSell,notSell);
-        }else{// buy==0
-            // either buy here
-            int currBuy=-arr[idx]+func(arr,idx+1,1,n,dp);
-            int notBuy=func(arr,idx+1,0,n,dp);
-            return dp[idx][buy]= Math.max(currBuy,notBuy);
-        }
-    }
+    
     public int maxProfit(int[] arr) {
         int n=arr.length;
-        int dp[][]=new int[n][2];
-        for(int i=0;i<n;i++){
-            Arrays.fill(dp[i],-1);
+        int dp[][]=new int[n+1][2];// one extra for base case 
+
+        //base case 
+        dp[n][0]=dp[n][1]=0;//no need by default but added
+        for(int idx=n-1;idx>=0;idx--){
+            for(int j=0;j<2;j++){
+                if(j==1){
+                    //either sell here 
+                    int currSell=arr[idx]+dp[idx+1][0];
+                    // or don't sell here
+                    int notSell=dp[idx+1][1];
+                    dp[idx][j]= Math.max(currSell,notSell);
+                }else{// buy==0
+                    // either buy here
+                    int currBuy=-arr[idx]+dp[idx+1][1];
+                    int notBuy=dp[idx+1][0];
+                    dp[idx][j]= Math.max(currBuy,notBuy);
+                }
+            }
         }
-        return func(arr,0,0,n,dp);
+        return dp[0][0];
     }
 }
