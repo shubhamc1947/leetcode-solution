879e5b906a5c777c4a4c9af7585a75a85bfae489
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-06-13 07:38:23 +0530

    Time: 36 ms (73.37%), Space: 44.5 MB (39.56%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,22 +1,25 @@
 class Solution {
-    
+    public int findMax(int arr[]){
+        int n=arr.length;
+        int maxi=Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+           maxi=Math.max(maxi,arr[i]);
+        }
+        return maxi;
+    }
     public int lengthOfLIS(int[] arr) {
         int n=arr.length;
-        int next[]=new int[n+1];
-        
-        for(int idx=n-1;idx>=0;idx--){
-            int curr[]=new int[n+1];
-            for(int prev=idx-1;prev>=-1;prev--){
-                int notTake=0+ next[prev+1];
-                int take=0;
-                if(prev==-1 || arr[idx]>arr[prev]){
-                    take=1+next[idx+1];
+        int dp[]=new int[n];
+        Arrays.fill(dp,1);
+        //i th index shows till 
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(arr[i]>arr[j]){
+                    dp[i]=Math.max(dp[i],dp[j]+1);
                 }
-                curr[prev+1]= Math.max(take,notTake);
             }
-            next=curr;
         }
 
-        return next[-1+1];
+        return findMax(dp);
     }
 }
