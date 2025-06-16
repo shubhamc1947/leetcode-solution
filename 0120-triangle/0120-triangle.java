351f0e568e22e2363450e0806775e035a381be73
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-06-16 21:22:10 +0530

    Time: 1 ms (99.62%), Space: 44.7 MB (21.5%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,21 +1,17 @@
 class Solution {
-   
+    public int func(List<List<Integer>> arr,int i,int j,int n,int dp[][]){
+        if(i==n-1) return arr.get(i).get(j);
+        if(dp[i][j]!=-1) return dp[i][j];
+        int bottom=func(arr,i+1,j,n,dp)+arr.get(i).get(j);
+        int dia=func(arr,i+1,j+1,n,dp)+arr.get(i).get(j);
+        return dp[i][j]= Math.min(bottom,dia);
+    }
     public int minimumTotal(List<List<Integer>> arr) {
         int n=arr.size();
-        int prev[]=new int[n];
-        for(int j=0;j<n;j++){
-            prev[j]=arr.get(n-1).get(j);
+        int dp[][]=new int[n][n];
+        for(int i=0;i<n;i++){
+            Arrays.fill(dp[i],-1);
         }
-        for(int i=n-2;i>=0;i--){
-            int curr[]=new int[i+1];
-            for(int j=0;j<=i;j++){
-                int bottom=prev[j]+arr.get(i).get(j);
-                int dia=prev[j+1]+arr.get(i).get(j);
-
-                curr[j]=Math.min(bottom,dia);
-            }
-            prev=curr;
-        }
-        return prev[0];
+        return func(arr,0,0,n,dp);
     }
 }
