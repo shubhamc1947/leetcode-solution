19c5c7d96a4194754b41973423169d434f31c343
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-06-16 09:10:34 +0530

    Time: 0 ms (100%), Space: 40.1 MB (96.63%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,13 +1,15 @@
 class Solution {
-    public int func(int n,int dp[]){
-        if(n==1 || n==2) return n;
-        if(dp[n]!=-1) return dp[n];
-        dp[n]= func(n-1,dp)+func(n-2,dp);
-        return dp[n];
-    }
+    
     public int climbStairs(int n) {
         int dp[]=new int[n+1];
-        Arrays.fill(dp,-1);
-        return func(n,dp);
+        // Arrays.fill(dp,-1);
+        if(n<=2) return n;
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3;i<=n;i++){
+            dp[i]= dp[i-1]+dp[i-2];
+        }
+        
+        return dp[n];
     }
 }
