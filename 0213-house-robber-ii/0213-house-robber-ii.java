e3eacaec69439889fe4c83646672b298095c0b37
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-06-16 12:54:46 +0530

    Time: 0 ms (100%), Space: 41 MB (65.47%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,36 +1,42 @@
 class Solution {
-    public int robfun(int[] arr) {
+    public static void printArr(int arr[]){
         int n=arr.length;
-        if(n==1) return arr[0];
-        if(n==2) return Math.max(arr[0],arr[1]);
-        int dp[]=new int[n];
-
-        //initilizatino 
-        int secondLast=arr[0];
-        int firstLast=Math.max(arr[0],arr[1]);
-
-        for(int i=2;i<n;i++){
-            int take=secondLast+arr[i];
-            int notTake=firstLast+0;
-            int curr=Math.max(take,notTake);
-            secondLast=firstLast;
-            firstLast=curr;
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
         }
-        return firstLast;
+        System.out.println("");
+    }
+    public int func(int arr[],int idx,int dp[]){
+        if(idx==0) return arr[0];
+        if(idx==1) return Math.max(arr[0],arr[1]);
+        if(dp[idx]!=-1) return dp[idx];
+        int take=func(arr,idx-2,dp)+arr[idx];
+        int notTake=func(arr,idx-1,dp)+0;
+        return dp[idx]= Math.max(take,notTake);
     }
     public int rob(int[] arr) {
         int n=arr.length;
-        //base case
         if(n==1) return arr[0];
         if(n==2) return Math.max(arr[0],arr[1]);
+        
+        int takeFirstEle[]=new int[n-1];
+        int takeLastEle[]=new int[n-1];
 
-        int firstTake[]=new int[n-1];
-        int lastTake[]=new int[n-1];
-        for (int i = 0; i < n - 1; i++) {
-                firstTake[i] = arr[i];
-                lastTake[i] = arr[i + 1];
+        for(int i=0;i<n-1;i++){
+            takeFirstEle[i]=arr[i];
+            takeLastEle[i]=arr[i+1];
         }
 
-        return Math.max(robfun(firstTake),robfun(lastTake));
+
+
+        int dp[]=new int[n-1];
+        Arrays.fill(dp,-1);
+        // printArr(takeFirstEle);
+        // printArr(takeLastEle);
+        int firstOne=func(takeFirstEle,n-2,dp);
+        Arrays.fill(dp,-1);
+        int lastOne=func(takeLastEle,n-2,dp);
+        return Math.max(firstOne,lastOne);
+
     }
 }
