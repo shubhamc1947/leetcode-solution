13123067a067848080be5193380d424ac5fdff2a
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-07-01 07:05:57 +0530

    Time: 0 ms (100%), Space: 41.9 MB (38.79%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,22 +1,18 @@
 class Solution {
+
     public int func(int arr[][],int i,int j,int dp[][]){
-        if(i==0 && j==0 ) return 1;
         if(i<0 || j<0 || arr[i][j]==1) return 0;
+        if(i==0 && j==0) return 1;
         if(dp[i][j]!=-1) return dp[i][j];
-        int left=func(arr,i,j-1,dp);
         int top=func(arr,i-1,j,dp);
-        return dp[i][j]= left+top;
+        int left=func(arr,i,j-1,dp);
+        return dp[i][j]= top+left;
     }
     public int uniquePathsWithObstacles(int[][] arr) {
-        
         int n=arr.length;
         int m=arr[0].length;
-
-       if(arr[0][0]==1) return 0;
         int dp[][]=new int[n][m];
-        for(int i=0;i<n;i++){
-            Arrays.fill(dp[i],-1);
-        }
+        for(int i=0;i<n;i++) Arrays.fill(dp[i],-1);
         return func(arr,n-1,m-1,dp);
     }
 }
