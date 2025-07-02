1405bf6347e873f998643111366e2970d8fd0691
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-07-02 07:30:22 +0530

    Time: 25 ms (72.41%), Space: 45.9 MB (67.72%) - LeetHub

--- 1559-cherry-pickup-ii/1559-cherry-pickup-ii.java
+++ 1559-cherry-pickup-ii/1559-cherry-pickup-ii.java
@@ -1,44 +1,38 @@
 class Solution {
-   
+    public int func(int i,int j1,int j2,int n,int m, int arr[][],int dp[][][]){
+        if(j1<0 ||j1>=m ||j2<0||j2>=m){
+            return 0;
+        }
+        if(i==n-1){
+            if(j1==j2){
+                return arr[i][j1];
+            }else{
+                return arr[i][j1]+arr[i][j2];
+            }
+        }
+        if(dp[i][j1][j2]!=-1) return dp[i][j1][j2];
+        int maxi=Integer.MIN_VALUE;
+        for(int x=-1;x<=1;x++){
+            for(int y=-1;y<=1;y++){ 
+                if(j1==j2){
+                    maxi=Math.max(maxi,arr[i][j1]+func(i+1,j1+x,j2+y,n,m,arr,dp));
+                }else{
+                    maxi=Math.max(maxi,arr[i][j1]+arr[i][j2]+func(i+1,j1+x,j2+y,n,m,arr,dp));
+                }
+            }
+        }
+        return dp[i][j1][j2]= maxi;
+    }
     public int cherryPickup(int[][] arr) {
         int n=arr.length;
         int m=arr[0].length;
-        int prev[][]=new int[m][m];
-
-        for(int j1=0;j1<m;j1++){
-            for(int j2=0;j2<m;j2++){
-                if(j1==j2) prev[j1][j2]=arr[n-1][j2];
-                else prev[j1][j2]=arr[n-1][j1]+arr[n-1][j2];
+        // i,j1,j2
+        int dp[][][]=new int[n][m][m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                Arrays.fill(dp[i][j],-1);
             }
         }
-
-        for(int i=n-2;i>=0;i--){
-            int curr[][]=new int[m][m];
-            for(int j1=0;j1<m;j1++){
-                for(int j2=0;j2<m;j2++){
-                    int maxi=-(int)1e8;
-                    
-                    for(int d1=-1;d1<=1;d1++){
-                        for(int d2=-1;d2<=1;d2++){
-                            int currVal=-(int)1e8;
-                            if(j1+d1>=0 && j1+d1<m && j2+d2>=0 && j2+d2<m){
-                                currVal=prev[j1+d1][j2+d2];
-
-                            }
-                            if(j1==j2){
-                                currVal+=arr[i][j1];
-                            }else{
-                                currVal+=arr[i][j1]+arr[i][j2];
-                            }
-                            maxi=Math.max(maxi,currVal);
-                        }
-                    }
-                    curr[j1][j2] = maxi;
-                }
-            }
-            prev=curr;
-        }
-
-        return prev[0][m-1];
+        return func(0,0,m-1,n,m,arr,dp);
     }
 }
