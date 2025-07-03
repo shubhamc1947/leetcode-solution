d55e19394971d49c4b6a5c44ef122f158944d099
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-07-03 22:05:46 +0530

    Time: 93 ms (23.07%), Space: 57.5 MB (26.31%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,40 +1,34 @@
 class Solution {
-
+    public boolean func(int arr[],int idx,int tar,int dp[][]){
+        if(tar==0) return true;
+        if(idx==0){
+            return false;
+        }
+        if(dp[idx][tar]!=-1) return dp[idx][tar]==1;
+        // not Take
+        boolean notTake=func(arr,idx-1,tar,dp);
+        boolean take=false;
+        if(tar>=arr[idx]){
+            take=func(arr,idx-1,tar-arr[idx],dp);
+        }
+        dp[idx][tar]= take||notTake?1:0;
+        return take||notTake;
+    }
     public boolean canPartition(int[] arr) {
-        int n=arr.length;
-
-        if(n==1) return false;
-
         int totSum=0;
+        int n=arr.length;
         for(int i=0;i<n;i++){
             totSum+=arr[i];
         }
-
-        if(totSum%2!=0) return false;
-
-        int target=totSum/2;
-        boolean dp[][]=new boolean[n][target+1];
-
-
+        if(totSum%2!=0){
+            return false;
+        }
+        int tar=totSum/2;
+        int idx=n-1;
+        int dp[][]=new int[n][tar+1];
         for(int i=0;i<n;i++){
-            dp[i][0]=true;
+            Arrays.fill(dp[i],-1);
         }
-        if(target>=arr[0]){
-            dp[0][arr[0]]=true;
-        }
-        
-        for(int i=1;i<n;i++){
-            for(int tar=1;tar<target+1;tar++){
-            boolean notTake=dp[i-1][tar];
-            boolean take=false;
-            if(tar>=arr[i]){
-
-                take=dp[i-1][tar-arr[i]];
-            }
-                dp[i][tar]=take||notTake;
-            }
-        }
-
-        return dp[n-1][target];
+        return func(arr,idx,tar,dp);
     }
 }
