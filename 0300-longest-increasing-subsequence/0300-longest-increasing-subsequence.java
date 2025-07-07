79ee6319737401e171a6b3519234026183636771
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-07-07 22:52:09 +0530

    Time: 386 ms (10.14%), Space: 76.3 MB (20.43%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,22 +1,19 @@
 class Solution {
-   
+    public int func(int arr[],int idx,int prev,int dp[][]){
+        //
+        if(idx==arr.length) return 0;
+        if(dp[idx][prev+1]!=-1) return dp[idx][prev+1];
+        int notTake=0+func(arr,idx+1,prev,dp);
+        int take=Integer.MIN_VALUE;
+        if(prev==-1|| arr[idx]>arr[prev]){
+            take=1+func(arr,idx+1,idx,dp);
+        }
+        return dp[idx][prev+1]= Math.max(take,notTake);
+    }
     public int lengthOfLIS(int[] arr) {
         int n=arr.length;
-        int dp[][]=new int[n+1][n+1];
-        for(int j=0;j<n+1;j++){
-            dp[n][j]=0;
-        }
-        for(int idx=n-1;idx>=0;idx--){
-            for(int prev=n-1;prev>=-1;prev--){
-                int notTake= dp[idx+1][prev+1];
-                //take
-                int take=0;
-                if(prev==-1 || arr[idx]>arr[prev]){
-                    take=1+ dp[idx+1][idx+1];
-                }
-                dp[idx][prev+1]= Math.max(take,notTake);
-            }
-        }
-        return dp[0][-1+1];
+        int dp[][]=new int[n][n];
+        for(int i=0;i<n;i++ ) Arrays.fill(dp[i],-1);
+        return func(arr,0,-1,dp);
     }
 }
