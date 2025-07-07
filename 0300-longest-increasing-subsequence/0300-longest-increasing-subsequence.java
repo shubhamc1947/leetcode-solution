46ea633a43b705166e5a9ae3733e24a3a117e646
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-07-07 09:39:57 +0530

    Time: 149 ms (18.13%), Space: 79.1 MB (12.47%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,24 +1,8 @@
 class Solution {
-    public int func(int arr[],int idx,int prev,int n,int dp[][]){
-        if(idx==n) return 0;
-        if(dp[idx][prev+1]!=-1) return dp[idx][prev+1];
-        // notTake
-        int notTake=func(arr,idx+1,prev,n,dp);
-        //take
-        int take=0;
-        if(prev==-1 || arr[idx]>arr[prev]){
-            take=1+func(arr,idx+1,idx,n,dp);
-        }
-        return dp[idx][prev+1]= Math.max(take,notTake);
-    }
+   
     public int lengthOfLIS(int[] arr) {
         int n=arr.length;
         int dp[][]=new int[n+1][n+1];
-        // idx=0->n-1 prev=-1,n-1
-        // for(int i=0;i<n;i++){
-        //     Arrays.fill(dp[i],-1);
-        // }
-
         for(int j=0;j<n+1;j++){
             dp[n][j]=0;
         }
