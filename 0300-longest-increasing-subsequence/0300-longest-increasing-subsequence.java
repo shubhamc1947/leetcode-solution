0e1646f44fe6c27277b59e654e086b01784cd1f5
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-07-08 07:10:29 +0530

    Time: 54 ms (29.72%), Space: 44.5 MB (47.06%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -7,8 +7,10 @@ class Solution {
         for(int i=0;i<n;i++){
             for(int j=0;j<i;j++){
                 if(arr[i]>arr[j]){
-                    dp[i]=Math.max(dp[i],dp[j]+1);
-                    ans=Math.max(dp[i],ans);
+                    if(dp[i]<dp[j]+1) {
+                        dp[i]=dp[j]+1;
+                        ans=Math.max(dp[i],ans);
+                    }
                 }
             }
         }
