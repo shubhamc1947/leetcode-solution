5dde09bf99839b29cadc962e1fa695f545830db9
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-07-10 17:21:31 +0530

    Time: 1 ms (78.56%), Space: 45.6 MB (93.83%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,27 +1,26 @@
 class Solution {
-    public int func(int arr[],int idx,int n,int buy,int dp[][]){
-        if(idx==n) return 0;
-
-        if(dp[idx][buy]!=-1) return dp[idx][buy];
-        //not take
-        int notTake=func(arr,idx+1,n,buy,dp);
-
-        //take
-        int take=-(int)1e8;
-        if(buy==0){
-             take=-arr[idx]+func(arr,idx+1,n,1,dp);
-        }else{
-             take=arr[idx]+func(arr,idx+1,n,0,dp);
-        }
-        return dp[idx][buy]= Math.max(take,notTake); 
-    }
+  
     public int maxProfit(int[] arr) {
         int n=arr.length;
         int dp[][]=new int[n+1][2];
 
-        for(int i=0;i<n;i++){
-            Arrays.fill(dp[i],-1);
+        for(int idx=n-1;idx>=0;idx--){
+            for(int buy=0;buy<=1;buy++){
+                int notTake= dp[idx+1][buy];
+
+                //take
+                int take=-(int)1e8;
+                if(buy==0){
+                        take=-arr[idx]+ dp[idx+1][1];
+                }else{
+                        take=arr[idx]+dp[idx+1][0];
+                }
+                dp[idx][buy]= Math.max(take,notTake);
+            }
         }
-        return func(arr,0,arr.length,0,dp);
+
+        // n-1=>0
+        
+        return dp[0][0];
     }
 }
