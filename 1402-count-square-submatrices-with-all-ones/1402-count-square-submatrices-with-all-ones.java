faf913719f15d3821288b6fcacc1bec9e005b08a
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-07-10 12:39:38 +0530

    Time: 5 ms (98.59%), Space: 54.9 MB (72.93%) - LeetHub

--- 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.java
+++ 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.java
@@ -2,29 +2,31 @@ class Solution {
     public int countSquares(int[][] arr) {
         int n=arr.length;
         int m=arr[0].length;
-        int dp[][]=new int[n][m];
-        for(int i=0;i<n;i++){
-            dp[i][0]=arr[i][0];
-        }
-        for(int j=1;j<m;j++){
-            dp[0][j]=arr[0][j];
+        int prev[]=new int[m];
+        // for(int i=0;i<n;i++){
+        //     dp[i][0]=arr[i][0];
+        // }
+        int sum=0;
+        for(int j=0;j<m;j++){
+            prev[j]=arr[0][j];
+            sum+=arr[0][j];
         }
         for(int i=1;i<n;i++){
+            int curr[]=new int[m];
+            curr[0]=arr[i][0];
+            sum+=curr[0];
             for(int j=1;j<m;j++){
                 if(arr[i][j]==1){
-                    dp[i][j]=Math.min(dp[i-1][j],Math.min(dp[i-1][j-1],dp[i][j-1]))+1;
+                    curr[j]=Math.min(prev[j],Math.min(prev[j-1],curr[j-1]))+1;
 
                 }else{
-                    dp[i][j]=0;
+                    curr[j]=0;
                 }
+                sum+=curr[j];
             }
+            prev=curr;
         }
-        int sum=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                sum+=dp[i][j];
-            }
-        }
+        
         return sum;
     }
 }
