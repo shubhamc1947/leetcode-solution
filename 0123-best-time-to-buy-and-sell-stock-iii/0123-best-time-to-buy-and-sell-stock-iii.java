7bacffd44a9ff20873bccbd584192d95d2638aa4
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-07-10 17:32:12 +0530

    Time: 99 ms (26.42%), Space: 79.8 MB (20.43%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,58 +1,30 @@
 class Solution {
-    public int func(int arr[],int i,int buy,int cap,int n,int dp[][][]){
-        if(cap==0)return 0;
-        if(i==n) return 0;
-        if(dp[i][buy][cap]!=-1) return dp[i][buy][cap];
-        if(buy==1){ // we can sell
-            int currSell=arr[i]+func(arr,i+1,0,cap-1,n,dp);
-            int notSell=func(arr,i+1,1,cap,n,dp);
-            return dp[i][buy][cap]= Math.max(currSell,notSell);
-        }else{ // we can buy buy=0
-            int currBuy=-arr[i]+func(arr,i+1,1,cap,n,dp);
-            int notBuy=func(arr,i+1,0,cap,n,dp);
-            return dp[i][buy][cap]= Math.max(currBuy,notBuy);
-        }
+    public int func(int arr[],int idx,int buy,int n,int k,int dp[][][]){
+        if(idx==n) return 0;
+        if(k==2) return 0;
+        if(dp[idx][buy][k]!=-1) return dp[idx][buy][k];
+        //nottake
+        int notTake=func(arr,idx+1,buy,n,k,dp);
 
+        //take
+        int take=-(int)1e9;
+        if(buy==0){
+            take= -arr[idx]+ func(arr,idx+1,1,n,k,dp);
+        }else{
+            take=arr[idx]+func(arr,idx+1,0,n,k+1,dp);
+        }
+        return dp[idx][buy][k]= Math.max(take,notTake);
     }
-    public int maxProfit(int[] arr) {    
+    public int maxProfit(int[] arr) {
+        // 0=>n
         int n=arr.length;
-        int ahead[][]=new int[2][3];// buy=>0,1 , cap=>2,1,0
-        
-        //base i==n ? j=0,1  k=2,1,0
-        // dp[n][0][2]=0;
-        // dp[n][1][2]=0;
-        // dp[n][0][1]=0;
-        // dp[n][1][1]=0;
-        // dp[n][0][0]=0;
-        // dp[n][1][0]=0;
-
-        // //base cap=0 ? i=0-> <=n and j=0,1 
-        // for(int i=0;i<=n;i++){
-        //     for(int j=0;j<2;j++){
-        //         dp[i][j][0]=0;
-        //     }
-        // }
-
-        for(int i=n-1;i>=0;i--){
-            int curr[][]=new int[2][3];
-            for(int buy=0;buy<2;buy++){
-                for(int cap=1;cap<3;cap++){//tricky as cap =0 return 0 so run for 1
-                    if(buy==1){ // we can sell
-                        int currSell=arr[i]+ahead[0][cap-1];
-                        int notSell=ahead[1][cap];
-                        curr[buy][cap]= Math.max(currSell,notSell);
-                    }else{ // we can buy buy=0
-                        int currBuy=-arr[i]+ ahead[1][cap];
-                        int notBuy= ahead[0][cap];
-                        curr[buy][cap]= Math.max(currBuy,notBuy);
-                    }
-                }
+        int k=3;
+        int dp[][][]=new int[n+1][2][k];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<2;j++){
+                Arrays.fill(dp[i][j],-1);
             }
-            ahead=curr;
         }
-
-
-        return ahead[0][2];
-
+        return func(arr,0,0,arr.length,0,dp);
     }
 }
