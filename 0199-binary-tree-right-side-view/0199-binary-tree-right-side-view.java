e4981d976d766d6f45a0bec71e2626d7a260e65d
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-09-10 23:04:24 +0530

    Time: 0 ms (100%), Space: 42 MB (81.38%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -13,32 +13,20 @@
  *     }
  * }
  */
- class Pair{
-    int x;
-    TreeNode node;
-    public Pair(int x, TreeNode node){
-        this.x=x;
-        this.node=node;
-    }
- }
 class Solution {
-    public List<Integer> rightSideView(TreeNode root) {
-        if(root==null) return new ArrayList<>();
-        TreeMap<Integer,Integer> tm=new TreeMap<>();
-        Queue<Pair> que=new LinkedList<>();
-        que.add(new Pair(0,root));
-        while(!que.isEmpty()){
-            Pair curr=que.remove();
-            int currX=curr.x;
-            TreeNode currN=curr.node;
-            tm.put(currX,currN.val);
-            if(currN.left!=null){
-                que.add(new Pair(currX+1,currN.left));
-            }
-            if(currN.right!=null){
-                que.add(new Pair(currX+1,currN.right));
-            }
+    public void preOrder(TreeNode root,int level, List<Integer> ans){
+        if(root==null){
+            return;
         }
-        return new ArrayList<>(tm.values());
+        if(ans.size()<level){
+            ans.add(root.val);
+        }
+        preOrder(root.right,level+1,ans);
+        preOrder(root.left,level+1,ans);
+    }
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> ans=new ArrayList<>();
+        preOrder(root,1,ans);
+        return ans;
     }
 }
