4af45bbc0618bf609e43d7a5e7885fa5db14b000
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-09-26 07:31:17 +0530

    Time: 1 ms (63.79%), Space: 41.9 MB (96.38%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -15,31 +15,32 @@
  */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        if(root==null) return new ArrayList<>();
-        List<List<Integer>>ans=new ArrayList<>();
+        List<List<Integer>> ans=new ArrayList<>();
+        if(root==null) return ans;
         Queue<TreeNode> que=new LinkedList<>();
         que.add(root);
-        boolean flag=false;//0-> L -> R and 1-> R-> L
+        boolean leftToRigth=true;
         while(!que.isEmpty()){
-            int size=que.size();
-            List<Integer> level=new ArrayList<>();
-            for(int i=1;i<=size;i++){
-                TreeNode curr=que.remove();
-                level.add(curr.val);
-                if(curr.left!=null){
-                    que.add(curr.left);
-                }
-                if(curr.right!=null){
-                    que.add(curr.right);
-                }
-            }
-            if(flag){
-                Collections.reverse(level);
 
+            int size=que.size();
+            Deque<Integer> currList=new LinkedList<>();
+            for(int i=0;i<size;i++){
+                TreeNode curr=que.remove();
+
+                if(leftToRigth==true){
+                    currList.addLast(curr.val);
+                }else{
+                    currList.addFirst(curr.val);
+                }
+
+                if(curr.left!=null) que.add(curr.left);
+                if(curr.right!=null) que.add(curr.right);
             }
-            ans.add(level);
-            flag=!flag;
+            leftToRigth=!leftToRigth;
+            ans.add(new ArrayList<>(currList));
         }
+
+
         return ans;
     }
 }
