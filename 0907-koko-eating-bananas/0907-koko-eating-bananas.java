4a6a6d3e19bd8b06f26f843c16417869c2c5f554
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-10-19 06:39:37 +0530

    Time: 37 ms (6.35%), Space: 45.8 MB (8.87%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.java
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.java
@@ -1,16 +1,28 @@
 class Solution {
-    public int minEatingSpeed(int[] piles, int H) {
-        int low = 1, high = 1000000000, k = 0;
-        while (low <= high) {
-            k = (low + high) / 2;
-            int h = 0;
-            for (int i = 0; i < piles.length; i ++) 
-                h += Math.ceil(1.0 * piles[i] / k);
-            if (h > H)
-                low = k + 1;
-            else
-                high = k - 1;
+    public boolean check(int arr[],int speed,int h){
+        int currTime=0;
+        for(int i=0;i<arr.length;i++){
+            currTime+= Math.ceil(1.0 * arr[i]/speed);
+            // System.out.println(Math.ceil(arr[i]/speed));
         }
-        return low;
+        // System.out.println(currTime);
+        if(currTime>h) return false;
+        return true;
+    }
+    public int minEatingSpeed(int[] arr, int h) {
+        int si=0,ei=1000000000;
+        int ans=0;
+
+        while(si<=ei){
+            int mid=si+((ei-si)/2);
+            System.out.println(mid);
+            if(check(arr,mid,h)){
+                ans=mid;
+                ei=mid-1;
+            }else{
+                si=mid+1;
+            }
+        }
+        return ans;
     }
 }
