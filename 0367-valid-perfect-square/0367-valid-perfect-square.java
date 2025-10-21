8154a9052e5116e67950cd56cadeae933cca8697
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-10-21 22:13:15 +0530

    Time: 0 ms (100%), Space: 38.9 MB (100%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.java
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -1,6 +1,16 @@
 class Solution {
-    public boolean isPerfectSquare(int n) {
-        int x=(int)Math.sqrt(n);
-        return x*x==n;
+    public boolean isPerfectSquare(int num) {
+       long si = 0, ei = num;
+        while (si <= ei) {
+            long mid = si + (ei - si) / 2;
+            if (mid * mid == num) {
+                return true;
+            } else if (mid * mid > num) {
+                ei = mid - 1;
+            } else {
+                si = mid + 1;
+            }
+        }
+        return false; 
     }
 }
