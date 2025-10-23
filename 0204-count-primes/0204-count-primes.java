d657491737e44530b5458e7c0ce73d4cf308abf8
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-10-23 07:22:08 +0530

    Time: 107 ms (27.02%), Space: 64.3 MB (8.4%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,10 +1,11 @@
 class Solution {
     public int countPrimes(int n) {
-        if(n<=1) return 0;
+        if(n<2) return 0;
+
         int arr[]=new int[n];
-        //0-> prime, 1-> noPrime
         arr[0]=1;
         arr[1]=1;
+        //1 -> non Prime, 0-> Prime
         int count=0;
         for(int i=2;i<n;i++){
             if(arr[i]==0){
