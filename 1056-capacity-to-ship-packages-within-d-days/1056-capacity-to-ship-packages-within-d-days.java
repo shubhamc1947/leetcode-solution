f6fe726d64bf4243504fe6882abe06524da73401
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-10-26 22:08:50 +0530

    Time: 13 ms (14.39%), Space: 47.3 MB (60.01%) - LeetHub

--- 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.java
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.java
@@ -1,35 +1,34 @@
 class Solution {
-    public boolean check(int arr[],int wt,int d,int n){
-
-        int currd=1;
-        int currWt=0;
-        for(int i=0;i<n;i++){
-            if(currWt+arr[i]>wt){
-                currd++;
-                currWt=arr[i];
-                if(currd>d){
+    public boolean check(int arr[],int days,int mid,int n){
+        int curr=arr[0];
+        int count=1;
+        for(int i=1;i<n;i++){
+            if(curr+arr[i]>mid){
+                count++;
+                curr=arr[i];
+                if(count>days){
                     return false;
                 }
             }else{
-                currWt+=arr[i];
+                curr+=arr[i];
             }
         }
-        
         return true;
     }
-    public int shipWithinDays(int[] arr, int d) {
+    public int shipWithinDays(int[] arr, int days) {
         int n=arr.length;
-        int si=0,ei=0;
-        for(int i=0;i<n;i++){
-            ei+=arr[i];
+        int si=arr[0];
+        int ei=arr[0];
+        for(int i=1;i<n;i++){
             si=Math.max(si,arr[i]);
+            ei+=arr[i];
         }
         int ans=ei;
         while(si<=ei){
             int mid=si+(ei-si)/2;
-            if(check(arr,mid,d,n)){
-                ans=mid;
+            if(check(arr,days,mid,n)){
                 ei=mid-1;
+                ans=mid;
             }else{
                 si=mid+1;
             }
