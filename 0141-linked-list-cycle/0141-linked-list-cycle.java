92586bfbf027cbc22c5626ba90e7631f6eb8962b
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-10-28 07:30:55 +0530

    Time: 0 ms (100%), Space: 44.2 MB (89.91%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -14,13 +14,10 @@ public class Solution {
         ListNode slow=head;
         ListNode fast=head;
         while(fast!=null && fast.next!=null){
-            fast=fast.next.next;
             slow=slow.next;
-            if(fast==slow){
-                return true;
-            }
+            fast=fast.next.next;
+            if(slow==fast) return true;
         }
         return false;
-
     }
 }
