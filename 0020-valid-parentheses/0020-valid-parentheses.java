11c3fbe205061ed27024065d64f6bba9f5f132be
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-11-02 11:24:19 +0530

    Time: 4 ms (27.28%), Space: 43.7 MB (5.36%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,16 +1,18 @@
 class Solution {
     public boolean isValid(String s) {
+        int n=s.length();
         Stack<Character> st=new Stack<>();
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='(' || s.charAt(i)=='{' || s.charAt(i)=='['){
+        for(int i=0;i<n;i++){
+            if(s.charAt(i)=='[' ||s.charAt(i)=='{' ||s.charAt(i)=='('){
                 st.push(s.charAt(i));
-            }else {
-                if(st.isEmpty()){
-                    return false;
-                }
-                char top=st.peek();
+            }else{
+                if(st.isEmpty()) return false;
 
-                if((top=='(' && s.charAt(i)==')') || (top=='{' && s.charAt(i)=='}') || (top=='[' && s.charAt(i)==']')){
+                if(s.charAt(i)==']' && st.peek()=='['){
+                    st.pop();
+                }else if(s.charAt(i)==')' && st.peek()=='('){
+                    st.pop();
+                }else if(s.charAt(i)=='}' && st.peek()=='{'){
                     st.pop();
                 }else{
                     return false;
