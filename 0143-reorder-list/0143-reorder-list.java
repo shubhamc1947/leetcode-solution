8e07757e779446aaea07c26c953563c812ed4823
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-11-07 06:31:23 +0530

    Time: 2 ms (85.8%), Space: 49.2 MB (6.66%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -11,8 +11,8 @@
 class Solution {
     public ListNode reverseLL(ListNode head){
         ListNode curr=head;
-        ListNode prev=null;
         ListNode next=null;
+        ListNode prev=null;
         while(curr!=null){
             next=curr.next;
             curr.next=prev;
@@ -22,8 +22,7 @@ class Solution {
         return prev;
     }
     public void reorderList(ListNode head) {
-        if(head==null || head.next==null) return;
-        //step 1 find middle
+        if(head==null || head.next==null) return ;
 
         ListNode slow=head;
         ListNode fast=head.next;
@@ -32,25 +31,21 @@ class Solution {
             slow=slow.next;
         }
 
-        //step2 store second list head and break the list and reverse the second LL
-
         ListNode head2=slow.next;
         slow.next=null;
-        head2=reverseLL(head2);
 
-        // reorder maintaining existing heads
+        head2=reverseLL(head2);
 
         ListNode temp1=head;
         ListNode temp2=head2;
+
         while(temp2!=null){
             ListNode next1=temp1.next;
             ListNode next2=temp2.next;
             temp1.next=temp2;
             temp2.next=next1;
-
             temp1=next1;
             temp2=next2;
         }
-        
     }
 }
