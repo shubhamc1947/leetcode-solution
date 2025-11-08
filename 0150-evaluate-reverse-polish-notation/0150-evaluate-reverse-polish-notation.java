67b0ad342829bc18f7880984e624b9d0f4370c5e
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-11-08 10:29:24 +0530

    Time: 7 ms (26.71%), Space: 45.3 MB (9.83%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,25 +1,23 @@
 class Solution {
-    public int evalRPN(String[] tokens) {
-        if(tokens.length < 3) return Integer.parseInt(tokens[0]);
-        Deque<Integer> s = new ArrayDeque<>();
-        for (int i = 0; i < tokens.length; i++) {
-            if (tokens[i].equals("/") || tokens[i].equals("+") || tokens[i].equals("-") || tokens[i].equals("*")) {
-                int b = s.pop();
-                int a = s.pop();
-                int result = 0;
-                if (tokens[i].equals("+")) {
-                    s.push(a + b);
-                } else if (tokens[i].equals("*")) {
-                    s.push(a * b);
-                } else if (tokens[i].equals("-")) {
-                    s.push(a - b);
-                } else if (tokens[i].equals("/")) {
-                    s.push(a / b);
+    public int evalRPN(String[] arr) {
+        Stack<Integer> st=new Stack<>();
+        for(int i=0;i<arr.length;i++){
+            if(arr[i].equals("+") || arr[i].equals("-")||arr[i].equals("*")||arr[i].equals("/")){
+                int a=st.pop();
+                int b=st.pop();
+                if(arr[i].equals("+")){
+                    st.push(b+a);
+                }else if(arr[i].equals("-")){
+                    st.push(b-a);
+                }else if(arr[i].equals("*")){
+                    st.push(b*a);
+                }else if(arr[i].equals("/")){
+                    st.push(b/a);
                 }
-            } else {
-                s.push(Integer.parseInt(tokens[i]));
+            }else{
+                st.push(Integer.parseInt(arr[i]));
             }
         }
-        return s.pop();
+        return st.pop();
     }
 }
