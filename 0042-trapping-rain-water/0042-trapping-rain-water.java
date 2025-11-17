c668eb248d49e66e594c2d774710d3371413959f
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-11-17 19:48:14 +0530

    Time: 0 ms (100%), Space: 47.9 MB (9.49%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,22 +1,21 @@
 class Solution {
     public int trap(int[] arr) {
         int n=arr.length;
-        int maxLeft=arr[0];
-        int maxiRight=arr[n-1];
+        int leftMax=arr[0];
+        int rightMax=arr[n-1];
         int si=0,ei=n-1;
         int sum=0;
         while(si<=ei){
             if(arr[si]<arr[ei]){
-                //left side 
-                maxLeft=Math.max(maxLeft,arr[si]);
-                sum+=maxLeft-arr[si];
+                leftMax=Math.max(leftMax,arr[si]);
+                sum+=leftMax-arr[si];
                 si++;
             }else{
-                maxiRight=Math.max(maxiRight,arr[ei]);
-                sum+=maxiRight-arr[ei];
+                rightMax=Math.max(rightMax,arr[ei]);
+                sum+=rightMax-arr[ei];
                 ei--;
             }
         }
-        return  sum;
+        return sum;
     }
 }
