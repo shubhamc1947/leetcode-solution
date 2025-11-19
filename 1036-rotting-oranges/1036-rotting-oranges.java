e65fcd8bbaef22cb12088e15d61fceaa94677d53
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-11-19 07:47:49 +0530

    Time: 2 ms (72.03%), Space: 44 MB (22.33%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -1,66 +1,54 @@
 class Solution {
-    class Pair{
+    class Node{
         int x;
         int y;
-        public Pair(int x, int y){
+        public Node (int x,int y){
             this.x=x;
             this.y=y;
         }
     }
-    public int orangesRotting(int[][] arr) {
-        int n=arr.length;
-        int m=arr[0].length;
-        int vis[][]=new int[n][m];
-        Queue<Pair> que=new LinkedList<>();
-        int freshCount=0;
+    public int orangesRotting(int[][] grid) {
+        int n=grid.length;
+        int m=grid[0].length;
+
+        //clone the array and count current status and Push rotton orange for t=Zero inside queue
+        Queue<Node> que=new LinkedList<>();
+        int cloneGrid[][]=new int[n][m];
+        int totalSafeOrange=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(arr[i][j]==2){
-                    que.add(new Pair(i,j));
-                    vis[i][j]=1;
+                if(grid[i][j]==2){
+                    cloneGrid[i][j]=2;
+                    que.add(new Node(i,j));
+                }else if(grid[i][j]==1){
+                    totalSafeOrange++;
+                    cloneGrid[i][j]=1;
                 }
-                if(arr[i][j]==1){
-                    freshCount++;
-                }
-
             }
         }
-        if(freshCount==0){
-            return 0;
-        }
-        if(que.isEmpty()){
-            return -1;
-        }
-        int timer=-1;
-        
-        // System.out.println(freshCount+" fresh count");
-        while(!que.isEmpty()){
-            timer++;
-            // System.out.println(que.size()+"-----------");
-            // if(que.size()==1){
-            //     System.out.println(que.peek().x+"--------"+que.peek().y+"---------");
-            // }
-            int size=que.size();
-            for(int z=1;z<=size;z++){
-                Pair curr=que.remove();
-                int currX=curr.x;
-                int currY=curr.y;
-                int xCor[]={-1,0,0,1};
-                int yCor[]={0,1,-1,0};
+        if(totalSafeOrange==0) return 0;
+        if(que.isEmpty()) return -1;
+        int xCor[]={-1,0,0,1};
+        int yCor[]={0,-1,1,0};
 
-                for(int p=0;p<4;p++){
-                    int nextX=xCor[p]+currX;
-                    int nextY=yCor[p]+currY;
-                    if(nextX>=0 && nextY>=0 && nextX<n && nextY<m && arr[nextX][nextY]==1 && vis[nextX][nextY]==0){
-                        // System.out.println(arr[nextX][nextY]+"--------"+nextX+"-----"+nextY);
-                        vis[nextX][nextY]=1;
-                        que.add(new Pair(nextX,nextY));
-                        freshCount--;
+        int time=-1;
+        while(!que.isEmpty()){
+            int currQueSize=que.size();
+            time++;
+            for(int z=0;z<currQueSize;z++){
+                Node currNode=que.remove();
+                for(int a=0;a<4;a++){
+                    int nextX=currNode.x+xCor[a];
+                    int nextY=currNode.y+yCor[a];
+                    if(nextX>=0 && nextY>=0 && nextX<n && nextY<m && cloneGrid[nextX][nextY]==1){
+                        cloneGrid[nextX][nextY]=2;
+                        que.add(new Node(nextX,nextY));
+                        totalSafeOrange--;
                     }
                 }
             }
         }
-        
-        return freshCount==0?timer:-1;
+        return totalSafeOrange==0?time:-1;
+
     }
 }
