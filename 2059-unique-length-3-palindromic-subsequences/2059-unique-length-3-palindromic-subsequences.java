b015e7e78431b1549de5e329931d083c42182ce8
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-11-21 07:57:49 +0530

    Time: 210 ms (34.65%), Space: 46.3 MB (33.74%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.java
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.java
@@ -1,28 +1,25 @@
 class Solution {
-    public int countPalindromicSubsequence(String inputString) {
-        int[] minExist = new int[26];
-        int[] maxExist = new int[26];
-        for (int i = 0; i < 26; i++) {
-            minExist[i] = Integer.MAX_VALUE;
-            maxExist[i] = Integer.MIN_VALUE;
-        }
-        for (int i = 0; i < inputString.length(); i++) {
-            int charIndex = inputString.charAt(i) - 'a';
-            minExist[charIndex] = Math.min(minExist[charIndex], i);
-            maxExist[charIndex] = Math.max(maxExist[charIndex], i);
-        }
-        int uniqueCount = 0;
-        for (int charIndex = 0; charIndex < 26; charIndex++) {
-            if (minExist[charIndex] == Integer.MAX_VALUE || maxExist[charIndex] == Integer.MIN_VALUE) {
-                continue; 
-            }
-            HashSet<Character> uniqueCharsBetween = new HashSet<>();
+    public int countPalindromicSubsequence(String s) {
+        int res = 0;
+        Set<Character> uniq = new HashSet<>();
 
-            for (int j = minExist[charIndex] + 1; j < maxExist[charIndex]; j++) {
-                uniqueCharsBetween.add(inputString.charAt(j));
-            }
-            uniqueCount += uniqueCharsBetween.size();
+        for (char c : s.toCharArray()) {
+            uniq.add(c);
         }
-        return uniqueCount;
+
+        for (char c : uniq) {
+            int start = s.indexOf(c);
+            int end = s.lastIndexOf(c);
+
+            if (start < end) {
+                Set<Character> charSet = new HashSet<>();
+                for (int i = start + 1; i < end; i++) {
+                    charSet.add(s.charAt(i));
+                }
+                res += charSet.size();
+            }
+        }
+
+        return res;        
     }
 }
