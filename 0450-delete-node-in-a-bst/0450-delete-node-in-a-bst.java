984bb03a07f0315f5251d27902f28214f1783a39
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-11-25 06:52:11 +0530

    Time: 0 ms (100%), Space: 46.7 MB (39.61%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -14,29 +14,35 @@
  * }
  */
 class Solution {
-    public TreeNode deleteNode(TreeNode root, int key) {
-        if (root == null) return null;
-
-        if (key < root.val) {
-            root.left = deleteNode(root.left, key);
-        } else if (key > root.val) {
-            root.right = deleteNode(root.right, key);
-        } else {
-            if (root.left == null && root.right == null) return null;
-            if (root.left == null) return root.right;
-            if (root.right == null) return root.left;
-
-            root.val = findMin(root.right);
-            root.right = deleteNode(root.right, root.val);
+    public int findMaxi(TreeNode root){
+        if(root.right!=null){
+            return findMaxi(root.right);
+        }else{
+            return root.val;
         }
-
-        return root;
     }
-
-    private int findMin(TreeNode root) {
-        while (root.left != null) {
-            root = root.left;
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if(root==null) return null;
+        if(root.val<key){
+            // go to right
+            root.right=deleteNode(root.right,key);
+        }else if(root.val>key){
+            root.left=deleteNode(root.left,key);
+        }else{
+            // found the  node
+            if(root.left==null && root.right==null){
+                return null;
+            }else if(root.left==null){
+                return root.right;
+            }else if(root.right==null){
+                return root.left;
+            }else{
+                // 
+                int maxi=findMaxi(root.left);
+                root.val=maxi;
+                root.left= deleteNode(root.left,maxi);
+            }
         }
-        return root.val;
+        return root;
     }
 }
