1065f7f9b98f2103b49864bfb44079760cf82235
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2025-12-03 08:55:47 +0530

    Time: 13 ms (78.45%), Space: 47.4 MB (84.2%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,30 +1,35 @@
+class Node{
+    int data;
+    int freq;
+    public Node(int data,int freq){
+        this.data=data;
+        this.freq=freq;
+    }
+}
 class Solution {
-    public int[] topKFrequent(int[] nums, int k) {
-        int n = nums.length;
-        int ans[] = new int[k];
-        int index = 0;
-        Map<Integer,Integer> map = new HashMap<>();
-        for(int i = 0;i<n;i++){
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+    public int[] topKFrequent(int[] arr, int k) {
+        //step 1 , find the frequency
+        int n=arr.length;
+        HashMap<Integer,Integer> hm=new HashMap<>();
+        for(int i=0;i<n;i++){
+            hm.put(arr[i],hm.getOrDefault(arr[i],0)+1);
         }
-        List<Integer>[] list = new List[n+1];
-        for(Map.Entry<Integer,Integer> e : map.entrySet()){
-            int freq = e.getValue();
-            if(list[freq] == null){
-                list[freq] = new ArrayList<>();
+
+        //step 2 , 
+        PriorityQueue<Node> pq=new PriorityQueue<>((a,b)->a.freq-b.freq);
+
+        for (int nums:hm.keySet()){
+            pq.offer(new Node(nums,hm.get(nums)));
+            if(pq.size()>k){
+                pq.poll();
             }
-            list[freq].add(e.getKey());
         }
-        for(int i = list.length-1;i>0 && index<k;i--){
-            if(list[i] != null){
-                for(int num : list[i]){
-                    ans[index++] = num;
-                    if(index == k){
-                        break;
-                    }
-                }
-            }
+        int ans[]=new int[pq.size()];
+        int i=0;
+        while(!pq.isEmpty()){
+            ans[i++]=pq.poll().data;
         }
         return ans;
+
     }
 }
