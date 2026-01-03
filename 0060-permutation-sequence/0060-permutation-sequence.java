6ac9f2b7fefa4cd894808f3be5c155a7dd151601
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2026-01-03 23:35:55 +0530

    Time: 2 ms (38.03%), Space: 43.2 MB (14.14%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.java
+++ 0060-permutation-sequence/0060-permutation-sequence.java
@@ -1,28 +1,27 @@
 class Solution {
     public int fact(int n){
         if(n==0 || n==1) return 1;
-        return n*fact(n-1);
+        return n* fact(n-1);
     }
-    public void helper(ArrayList<Integer> arr,int n,int k,String res[]){
-        if(arr.size()==0){
-            return;
-        }
-        int factn=fact(n);
-        int idx=k/factn;
-        res[0]=res[0]+""+arr.get(idx);
-        arr.remove(idx);
-        helper(arr,n-1,k%factn,res);
-
-    }
+    
     public String getPermutation(int n, int k) {
-        String res[]=new String[1];
-        res[0]="";
+        String res="";
         ArrayList<Integer>arr=new ArrayList<>();
         for(int i=1;i<=n;i++){
             arr.add(i);
         }
-
-        helper(arr,n-1,k-1,res);
-        return res[0];
+        int factn=fact(n-1);
+        k=k-1;
+        while(true){
+            int idx=k/factn;
+            res=res+""+arr.get(idx);
+            arr.remove(idx);
+            if(arr.size()==0){
+                break;
+            }
+            k=k%factn;
+            factn=factn/arr.size();
+        }
+        return res;
     }
 }
