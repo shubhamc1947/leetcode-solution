b7624f7a63dd02885610a3179d5b959349551b1d
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2026-01-04 17:26:38 +0530

    Time: 2 ms (95.36%), Space: 46.1 MB (14.62%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,23 +1,25 @@
 class Solution {
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> list = new ArrayList<>();
-        process(candidates,target,list,0,new ArrayList<>());
-        return list;
+    public void helper(int idx,int n,int currSum,int tar, int arr[],ArrayList<Integer> curr,List<List<Integer>> res){
+        if(currSum==tar){
+            res.add(new ArrayList<>(curr));
+            return;
+        }
+        if(currSum>tar || idx==n) return;
+        //take
+        curr.add(arr[idx]);
+        currSum+=arr[idx];
+        helper(idx,n,currSum,tar,arr,curr,res);
+        currSum-=arr[idx];
+        curr.remove(curr.size()-1);
+
+        //not take
+        helper(idx+1,n,currSum,tar,arr,curr,res);
     }
-    static void process(int[] candidates, int target,List<List<Integer>> list,int start,ArrayList<Integer> arr){
-        if(target == 0){
-            list.add(new ArrayList<>(arr));
-            return;
-        }
-
-        if(target < 0){
-            return;
-        }
-
-        for(int i=start;i<candidates.length;i++){
-            arr.add(candidates[i]);
-            process(candidates,target - candidates[i],list,i,arr);
-            arr.removeLast();
-        }
+    public List<List<Integer>> combinationSum(int[] arr, int tar) {
+        List<List<Integer>> res=new ArrayList<>();
+        int idx=0;
+        int n=arr.length;
+        helper(idx,n,0,tar,arr,new ArrayList<>(),res);
+        return res;
     }
 }
