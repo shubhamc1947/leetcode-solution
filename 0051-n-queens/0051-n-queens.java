b2afdf1e296fe7bca9b0946e586e2f9f50e6b8f5
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2026-01-07 07:58:04 +0530

    Time: 2 ms (79.61%), Space: 46.7 MB (46.65%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,43 +1,61 @@
 class Solution {
-    public  List<String> construct(char board[][],int n){
-		List<String> curr=new ArrayList<>();
-		for(int i=0;i<n;i++){
-			curr.add(new String(board[i]));
-		}
-		return curr;
-	}
-	public void solveNQueensHelper(int col,int n,char board[][],List<List<String>> ans,boolean rowHash[],boolean lowerDiaHash[],boolean upperDiaHash[]){
-		if(col==n){
-			ans.add(construct(board,n));
-		}
-		
-		for(int row=0;row<n;row++){
-			if(rowHash[row]==false && lowerDiaHash[row+col]==false && upperDiaHash[(n-1)+(col-row)]==false){
-				board[row][col]='Q';
-				rowHash[row]=true;
-				lowerDiaHash[row+col]=true;
-				upperDiaHash[(n-1)+(col-row)]=true;
-				
-				solveNQueensHelper(col+1,n,board,ans,rowHash,lowerDiaHash,upperDiaHash);
-				board[row][col]='.';
-				rowHash[row]=false;
-				lowerDiaHash[row+col]=false;
-				upperDiaHash[(n-1)+(col-row)]=false;
-			}
-		}
-		
-	}
-	public List<List<String>> solveNQueens(int n) {
-        List<List<String>> ans=new ArrayList<>();
-		char board[][]=new char[n][n];
-		for(int i=0;i<n;i++){
-			Arrays.fill(board[i],'.');
-		}
-		boolean rowHash[]=new boolean[n];
-		boolean lowerDiaHash[]=new boolean[2*n-1];
-		boolean upperDiaHash[]=new boolean[2*n-1];
-		
-		solveNQueensHelper(0,n,board,ans,rowHash,lowerDiaHash,upperDiaHash);
-		return ans;
+    private boolean isSafe(int row,int col, char board[][], int n,int m){
+        //left
+        int currRow=row;
+        int currCol=col-1;
+        while(currCol>=0){
+            if(board[currRow][currCol]=='Q') return false;
+            currCol--;
+        }
+        //top left
+        currRow=row-1;
+        currCol=col-1;
+        while(currRow>=0 && currCol>=0){
+            if(board[currRow][currCol]=='Q') return false;
+            currRow--;
+            currCol--;
+        }
+
+        currRow=row+1;
+        currCol=col-1;
+        while(currRow<n && currCol>=0){
+            if(board[currRow][currCol]=='Q') return false;
+            currRow++;
+            currCol--;
+        }
+
+        return true;
+    }
+    private List<String> constructSolution(char board[][],int n, int m){
+        List<String> curr=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            curr.add(new String(board[i]));
+        }
+        return curr;
+    }
+
+    private void helper(char board[][],int col,int n,int m,List<List<String>> res){
+        if(col==m){
+            res.add(new ArrayList<>(constructSolution(board,n,m)));
+            return;
+        }
+            for(int i=0;i<n;i++){
+                if(isSafe(i,col,board,n,m)){
+                    board[i][col]='Q';
+                    helper(board,col+1,n,m,res);
+                    board[i][col]='.';
+                }
+            }
+        
+    }
+
+    public List<List<String>> solveNQueens(int n) {
+        char board[][]=new char[n][n];
+        for(int i=0;i<n;i++){
+            Arrays.fill(board[i],'.');
+        }
+        List<List<String>> res=new ArrayList<>();
+        helper(board,0,n,n,res);
+        return res;
     }
 }
