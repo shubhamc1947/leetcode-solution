22c23c5a8160ab260dee510fcc3eb59e475cdfcc
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2026-01-11 15:59:22 +0530

    Time: 0 ms (100%), Space: 42 MB (57.3%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,16 +1,13 @@
 class Solution {
-    public int helper(int n,int dp[]){
-        // if(n==0) return 1;// no stair, don't write, keep it for 1 and 2 stairs, as at n==0, returning 1 makes no sense
-        if(n==1) return 1;
-        if(n==2) return 2;
-        if(dp[n]!=-1) return dp[n];
-        dp[n]= helper(n-1,dp)+helper(n-2,dp);
-        return dp[n];
-    }
     public int climbStairs(int n) {
+        if(n<=3) return n;
         int dp[]=new int[n+1];
         Arrays.fill(dp,-1);
-        return helper(n,dp);
-        
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 }
