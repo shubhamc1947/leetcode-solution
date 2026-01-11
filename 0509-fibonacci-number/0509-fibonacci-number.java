24cacc775eae6baabcd7d63b33849296c69a46db
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2026-01-11 15:42:23 +0530

    Time: 0 ms (100%), Space: 42 MB (54.8%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,14 +1,13 @@
 class Solution {
-    public int fiboHelper(int i,int dp[]){
-		if(i<=1) return i;
-		if(dp[i]!=-1) return dp[i];
-		dp[i]=fiboHelper(i-1,dp)+fiboHelper(i-2,dp);
-		return dp[i];
-	}
     public int fib(int n) {
         int dp[]=new int[n+1];
+        if(n<=1) return n;
         Arrays.fill(dp,-1);
-        fiboHelper(n,dp);
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
         return dp[n];
     }
 }
