4e5cd4dac7a47c205a26051bfd9d842e717136d5
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2026-01-11 13:48:11 +0530

    Time: 2 ms (95.28%), Space: 46.1 MB (12.12%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,24 +1,24 @@
 class Solution {
-    public void helper(int arr[],int idx,int n,int tar,List<Integer> curr,List<List<Integer>> res){
+    public void helper(int idx,int tar, int arr[],List<Integer> curr, List<List<Integer>> ans){
+        if(tar<0 || idx>=arr.length) return;
         if(tar==0){
-            res.add(new ArrayList<>(curr));
+            ans.add(new ArrayList<>(curr));
             return;
         }
-        if(idx==n || tar<0){
-            return;
-        }
-        //take 
-        tar=tar-arr[idx];
+
+        //take
         curr.add(arr[idx]);
-        helper(arr,idx,n,tar,curr,res);
-        tar=tar+arr[idx];
+        tar-=arr[idx];
+        helper(idx,tar,arr,curr,ans);
+        tar+=arr[idx];
         curr.remove(curr.size()-1);
 
-        helper(arr,idx+1,n,tar,curr,res);
+        //not take
+        helper(idx+1,tar,arr,curr,ans);
     }
     public List<List<Integer>> combinationSum(int[] arr, int tar) {
-        List<List<Integer>> res= new ArrayList<>();
-        helper(arr,0,arr.length,tar,new ArrayList<>(),res);
-        return res;
+        List<List<Integer>> ans=new ArrayList<>();
+        helper(0,tar,arr,new ArrayList<>(),ans);
+        return ans;
     }
 }
