6c331e559a00a2d1fd6ee9517ac6566ab60719b9
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2026-01-12 09:09:12 +0530

    Time: 0 ms (100%), Space: 42.7 MB (77.93%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,19 +1,18 @@
 class Solution {
-    public int helper(int arr[],int idx,int dp[]){
-        if(idx==0) return arr[0];
-        if(idx==1) return Math.max(arr[0],arr[1]);
-        if(dp[idx]!=-1) return dp[idx];
-        //take
-        int take=helper(arr,idx-2,dp)+arr[idx];
-        //nottake
-        int notTake=helper(arr,idx-1,dp)+0;
-        dp[idx]=Math.max(take,notTake);
-        return dp[idx];
-    }
+
     public int rob(int[] arr) {
         int n=arr.length;
+        if(n==1) return arr[0];
+        if(n==2) return Math.max(arr[0],arr[1]);
         int dp[]=new int[n];
         Arrays.fill(dp,-1);
-        return helper(arr,arr.length-1,dp);
-    }
+        dp[0]=arr[0];
+        dp[1]=Math.max(arr[0],arr[1]);
+        for(int idx=2;idx<n;idx++){
+            int take=dp[idx-2]+arr[idx];
+            int notTake=dp[idx-1]+0;
+            dp[idx]=Math.max(take,notTake);
+        }
+        return dp[n-1];
+    }   
 }
