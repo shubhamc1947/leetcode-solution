fd594a54c0bd7a9fd31796e246adb8bab9baa22a
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2026-01-17 20:55:52 +0530

    Time: 0 ms (100%), Space: 43.7 MB (30.92%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -4,25 +4,28 @@ class Solution {
         int n=arr.length;
         int m=arr[0].length;
         if(arr[n-1][m-1]==1) return 0;
-        int dp[][]=new int[n][m];
+        
+        int prev[]=new int[m];
+        Arrays.fill(prev,-1);
+
         for(int i=0;i<n;i++){
-            Arrays.fill(dp[i],-1);
-        }
-        for(int i=0;i<n;i++){
+            int temp[]=new int[m];
             for(int j=0;j<m;j++){
                 if(i==0 && j==0){
-                    dp[i][j]=1;
+                    temp[j]=1;
                 }else if(arr[i][j]==1){
-                    dp[i][j]=0;
+                    temp[j]=0;
                 }else{
                     int left=0;
                     int top=0;
-                    if(i>0) top=dp[i-1][j];
-                    if(j>0) left=dp[i][j-1];
-                    dp[i][j]=left+top;
+                    if(i>0) top=prev[j];
+                    if(j>0) left=temp[j-1];
+                    temp[j]=left+top;
                 }
             }
+            prev=temp;
         }
-        return dp[n-1][m-1];
+
+        return prev[m-1];
     }
 }
