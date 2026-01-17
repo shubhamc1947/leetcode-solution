1f4c276cd81ec71192ba9afa2f26b4bca0c67217
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2026-01-17 12:49:55 +0530

    Time: 0 ms (100%), Space: 42.9 MB (47.11%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,31 +1,49 @@
 class Solution {
-    public int helper(int arr[],int idx,int dp[]){
-        if(idx==0) return arr[0];
-        if(idx==1) return Math.max(arr[0],arr[1]);
-        if(dp[idx]!=-1) return dp[idx];
-        //take
-        int take=helper(arr,idx-2,dp)+arr[idx];
-        //nottake
-        int notTake=helper(arr,idx-1,dp)+0;
-        dp[idx]=Math.max(take,notTake);
-        return dp[idx];
-    }
     public int rob(int[] arr) {
         int n=arr.length;
-        if(n==1) return arr[0];
-        if(n==2) return Math.max(arr[0],arr[1]);
-        int takeFirstEle[]=new int[n-1];
-        int takeSecondElement[]=new int[n-1];
-
-        for(int i=0;i<n-1;i++){
-            takeFirstEle[i]=arr[i];
-            takeSecondElement[i]=arr[i+1];
+        if(n==1){
+            return arr[0];
+        }else if(n==2){
+            return Math.max(arr[0],arr[1]);
         }
-        int dp[]=new int[n-1];
-        Arrays.fill(dp,-1);
-        int first=helper(takeFirstEle,n-2,dp);
-        Arrays.fill(dp,-1);
-        int second=helper(takeSecondElement,n-2,dp);
-        return Math.max(first,second);
+
+        //step 1 , breaking into two differ arr
+        int arr1[]=new int[n-1];
+        int arr2[]=new int[n-1];
+        int len1=n-1;
+        int len2=n-1;
+        for(int i=0;i<n-1;i++){
+            arr1[i]=arr[i];
+            arr2[i]=arr[i+1];
+        }
+
+        // now let's consider the answer for these two in the optimize wayonly
+        //first arr
+        int prev2=arr1[0];
+        int prev1=Math.max(arr1[0],arr1[1]);
+        
+        for(int idx=2;idx<len1;idx++){
+            int take=arr1[idx]+prev2;
+            int notTake=0+prev1;
+            int curr=Math.max(take,notTake);
+
+            prev2=prev1;
+            prev1=curr;
+        }
+
+        int ans=prev1;
+
+        //second arr
+        prev2=arr2[0];
+        prev1=Math.max(arr2[0],arr2[1]);
+        for(int idx=2;idx<len2;idx++){
+            int take=arr2[idx]+prev2;
+            int notTake=0+prev1;
+            int curr=Math.max(take,notTake);
+            prev2=prev1;
+            prev1=curr;
+        }
+
+        return Math.max(ans,prev1);
     }
 }
