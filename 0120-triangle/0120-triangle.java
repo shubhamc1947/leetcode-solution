bd77cd83d96a90deb3791e5b1bcd07ebfa715703
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2026-01-17 22:04:15 +0530

    Time: 5 ms (42.87%), Space: 45.8 MB (40.63%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,20 +1,29 @@
 class Solution {
- 
-    public int minimumTotal(List<List<Integer>> arr) {
-        int n=arr.size();
-        int prev[]=new int[n];
+    public int helper(List<List<Integer>> tri,int i,int j,int n, int dp[][]){
+        if(i==n-1) return tri.get(i).get(j);
+        if(dp[i][j]!=-1) return dp[i][j];
+        int bottom= tri.get(i).get(j)+helper(tri,i+1,j,n,dp);
+        int dia=tri.get(i).get(j)+helper(tri,i+1,j+1,n,dp);
+        return dp[i][j]= Math.min(bottom,dia);
+    }
+    public int minimumTotal(List<List<Integer>> tri) {
+        int n=tri.size();
+        int dp[][]=new int[n][n];
+        for(int i=0;i<n;i++){
+            Arrays.fill(dp[i],-1);
+        }
+        // return helper(tri,0,0,n,dp);
         for(int j=0;j<n;j++){
-            prev[j]=arr.get(n-1).get(j);
+            dp[n-1][j]=tri.get(n-1).get(j);
         }
         for(int i=n-2;i>=0;i--){
-            int curr[]=new int[n];
             for(int j=0;j<=i;j++){
-                int bottom=arr.get(i).get(j)+prev[j];
-                int dia=arr.get(i).get(j)+prev[j+1];
-                curr[j]= Math.min(bottom,dia);
+                int bottom= tri.get(i).get(j)+dp[i+1][j];
+                int dia=tri.get(i).get(j)+dp[i+1][j+1];
+                dp[i][j]= Math.min(bottom,dia);
             }
-            prev=curr;
         }
-        return prev[0];
+
+        return dp[0][0];
     }
 }
