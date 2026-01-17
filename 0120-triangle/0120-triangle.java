625ec7942d3fd3cf533eca86ad4ae963791561f0
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2026-01-17 22:05:32 +0530

    Time: 5 ms (42.87%), Space: 45.5 MB (57.35%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -8,22 +8,21 @@ class Solution {
     }
     public int minimumTotal(List<List<Integer>> tri) {
         int n=tri.size();
-        int dp[][]=new int[n][n];
-        for(int i=0;i<n;i++){
-            Arrays.fill(dp[i],-1);
-        }
-        // return helper(tri,0,0,n,dp);
+        
+        int prev[]=new int[n];
         for(int j=0;j<n;j++){
-            dp[n-1][j]=tri.get(n-1).get(j);
+            prev[j]=tri.get(n-1).get(j);
         }
+        int temp[]=new int[n];
         for(int i=n-2;i>=0;i--){
             for(int j=0;j<=i;j++){
-                int bottom= tri.get(i).get(j)+dp[i+1][j];
-                int dia=tri.get(i).get(j)+dp[i+1][j+1];
-                dp[i][j]= Math.min(bottom,dia);
+                int bottom= tri.get(i).get(j)+prev[j];
+                int dia=tri.get(i).get(j)+prev[j+1];
+                temp[j]= Math.min(bottom,dia);
             }
+            prev=temp;
         }
 
-        return dp[0][0];
+        return prev[0];
     }
 }
