5595d1f338bd10cf914ad69289faf2d5d5b9749c
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2026-01-17 20:53:51 +0530

    Time: 1 ms (18.35%), Space: 43.7 MB (30.92%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,14 +1,4 @@
 class Solution {
-    public int helper(int arr[][],int i,int j,int dp[][]){
-        if(i==0 && j==0) return 1;
-        if(i<0 || j<0 || arr[i][j]==1) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        int left=helper(arr,i,j-1,dp);
-        int top=helper(arr,i-1,j,dp);
-
-        return dp[i][j] = left+top;
-
-    }
     public int uniquePathsWithObstacles(int[][] arr) {
         if(arr[0][0]==1) return 0;
         int n=arr.length;
@@ -18,7 +8,21 @@ class Solution {
         for(int i=0;i<n;i++){
             Arrays.fill(dp[i],-1);
         }
-
-        return helper(arr,n-1,m-1,dp);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0 && j==0){
+                    dp[i][j]=1;
+                }else if(arr[i][j]==1){
+                    dp[i][j]=0;
+                }else{
+                    int left=0;
+                    int top=0;
+                    if(i>0) top=dp[i-1][j];
+                    if(j>0) left=dp[i][j-1];
+                    dp[i][j]=left+top;
+                }
+            }
+        }
+        return dp[n-1][m-1];
     }
 }
