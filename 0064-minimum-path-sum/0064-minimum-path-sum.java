1e43cb257fe01158974466b81495da2e8b9c003e
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2026-01-17 21:40:26 +0530

    Time: 6 ms (10.88%), Space: 50.4 MB (25.8%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,29 +1,33 @@
 class Solution {
-    
+    public int helper(int arr[][],int i,int j, int dp[][]){
+        if(i==0 && j==0)return arr[i][j];
+        if(i<0 || j<0) return Integer.MAX_VALUE;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int left=helper(arr,i,j-1,dp);
+        int top=helper(arr,i-1,j,dp);
+        return dp[i][j] = arr[i][j]+Math.min(left,top);
+    }
     public int minPathSum(int[][] arr) {
         int n=arr.length;
         int m=arr[0].length;
-        int prev[]=new int[m];
+        int dp[][]=new int[n][m];
         for(int i=0;i<n;i++){
-            int curr[]=new int[m];
+            Arrays.fill(dp[i],-1);
+        }
+        for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(i==0 && j==0){
-                    curr[j]=arr[i][j];
+                    dp[i][j]=arr[i][j];
                 }else{
-                    int top=(int)1e8;
-                    if(i-1>=0){
-                        top=arr[i][j]+prev[j];
-                    }
-                    int left=(int)1e8;
-                    if(j-1>=0){
-                        left=arr[i][j]+curr[j-1];
-                    }
-                    curr[j]=Math.min(left,top);
+                    int left=Integer.MAX_VALUE;
+                    int top=Integer.MAX_VALUE;
+                    if(i>0) top=dp[i-1][j];
+                    if(j>0) left=dp[i][j-1];
+
+                    dp[i][j]=arr[i][j]+Math.min(left,top);
                 }
             }
-            prev=curr;
-
         }
-        return prev[m-1];
+        return dp[n-1][m-1];
     }
 }
