10eb34a39f7f88ef39dc872a12e031c107e237fa
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2026-01-18 15:15:33 +0530

    Time: 14 ms (37.28%), Space: 45.7 MB (13.98%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
@@ -1,17 +1,5 @@
 class Solution {
-    int MOD = 1000000007;
-
-    public int helper(int i,int k,int tar,int dp[][]){
-        if(i==0) return tar==0?1:0;
-        if(tar<0) return 0;
-        if(dp[i][tar]!=-1) return dp[i][tar];
-        int way=0;
-        for(int j=1;j<=k;j++){
-            way=(way+helper(i-1,k,tar-j,dp))%MOD;
-        }
-
-        return dp[i][tar]= way;
-    }
+    int MOD=1000000007;
     public int numRollsToTarget(int n, int k, int target) {
         int dp[][]=new int[n+1][target+1];
         
@@ -19,21 +7,25 @@ class Solution {
         // n=>0
         // target target=>0
         dp[0][0]=1;
+        int prev[]=new int[target+1];
+        prev[0]=1;
         for(int i=1;i<=n;i++){
+            int temp[]=new int[target+1];
             for(int tar=target;tar>=0;tar--){
                 int way=0;
                 
                 for(int j=1;j<=k;j++){
                     if(tar-j>=0){
-                        way=(way+ dp[i-1][tar-j])%MOD;
+                        way=(way+ prev[tar-j])%MOD;
                     }
                 }
 
-                dp[i][tar]= way;
+                temp[tar]= way;
 
             }
+            prev=temp;
         }
-        return dp[n][target];
+        return prev[target];
     }
 
 }
