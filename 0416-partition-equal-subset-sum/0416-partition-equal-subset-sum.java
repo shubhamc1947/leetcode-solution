178a77637d2e77b2a6622dbefc24cfc289c10edd
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2026-01-20 08:29:33 +0530

    Time: 62 ms (40.52%), Space: 47.9 MB (48.39%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,19 +1,4 @@
 class Solution {
-    public boolean func(int arr[],int idx,int tar,int dp[][]){
-        if(tar==0) return true;
-        if(idx==0){
-            return false;
-        }
-        if(dp[idx][tar]!=-1) return dp[idx][tar]==1;
-        // not Take
-        boolean notTake=func(arr,idx-1,tar,dp);
-        boolean take=false;
-        if(tar>=arr[idx]){
-            take=func(arr,idx-1,tar-arr[idx],dp);
-        }
-        dp[idx][tar]= take||notTake?1:0;
-        return take||notTake;
-    }
     public boolean canPartition(int[] arr) {
         int totSum=0;
         int n=arr.length;
