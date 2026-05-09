5a40eb9108c8b1e79132d223af9270b303d9ce17
Shubham Chaturvedi <70830132+shubhamc1947@users.noreply.github.com>
2026-05-09 07:17:00 +0530

    Time: 4 ms (62.69%), Space: 47.7 MB (16.42%) - LeetHub

---
+++ 1914-cyclically-rotating-a-grid/1914-cyclically-rotating-a-grid.java
@@ -0,0 +1,65 @@
+class Solution {
+    public int[][] rotateGrid(int[][] grid, int k) {
+
+        int m = grid.length;
+        int n = grid[0].length;
+
+        int layers = Math.min(m, n) / 2;
+
+        for (int layer = 0; layer < layers; layer++) {
+
+            ArrayList<Integer> nums = new ArrayList<>();
+
+            int top = layer;
+            int bottom = m - layer - 1;
+            int left = layer;
+            int right = n - layer - 1;
+
+            for (int j = left; j <= right; j++) {
+                nums.add(grid[top][j]);
+            }
+
+            for (int i = top + 1; i <= bottom - 1; i++) {
+                nums.add(grid[i][right]);
+            }
+
+            for (int j = right; j >= left; j--) {
+                nums.add(grid[bottom][j]);
+            }
+
+            for (int i = bottom - 1; i >= top + 1; i--) {
+                nums.add(grid[i][left]);
+            }
+
+            int len = nums.size();
+
+            int rotate = k % len;
+
+            int[] rotated = new int[len];
+
+            for (int i = 0; i < len; i++) {
+                rotated[i] = nums.get((i + rotate) % len);
+            }
+
+            int idx = 0;
+
+            for (int j = left; j <= right; j++) {
+                grid[top][j] = rotated[idx++];
+            }
+
+            for (int i = top + 1; i <= bottom - 1; i++) {
+                grid[i][right] = rotated[idx++];
+            }
+
+            for (int j = right; j >= left; j--) {
+                grid[bottom][j] = rotated[idx++];
+            }
+
+            for (int i = bottom - 1; i >= top + 1; i--) {
+                grid[i][left] = rotated[idx++];
+            }
+        }
+
+        return grid;
+    }
+}
